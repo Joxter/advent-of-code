022e89bc2962d088e7f9c2653eea5565d79af308
Joxter <joxterrr@gmail.com>
2022-12-06 20:55:54 +0100

    day 6 alter solution

--- day-06/task.js
+++ day-06/task.js
@@ -11,6 +11,9 @@ console.log('answer: ', part1(inputData));
 console.log('test2: ', part2(testInput));
 console.log('answer2: ', part2(inputData));
 
+console.log('alter(4) OK: ', part1alter(inputData));
+console.log('alter(14) OK: ', part2alter(inputData));
+
 function part1(inp) {
   return inp.split('\n').map((line) => {
     let marker = line.slice(0, 3).split('');
@@ -37,3 +40,42 @@ function part2(inp) {
   });
 }
 
+function part1alter(inp) {
+  return inp.split('\n').map((line) => {
+    return noSetSolution(4, line);
+  });
+}
+
+function part2alter(inp) {
+  return inp.split('\n').map((line) => {
+    return noSetSolution(14, line);
+  });
+}
+
+function noSetSolution(len, line) {
+  let marker = Array('z'.charCodeAt(0) + 1).fill(0);
+  let unique = 0;
+
+  for (let i = 0; i < len; i++) {
+    let charCode = line[i].charCodeAt(0);
+
+    if (marker[charCode] === 0) unique++;
+    if (marker[charCode] === 1) unique--;
+    marker[charCode]++;
+  }
+  if (unique === len) return len;
+
+  for (let i = len; i < line.length; i++) {
+    let charCode = line[i].charCodeAt(0);
+    if (marker[charCode] === 0) unique++;
+    if (marker[charCode] === 1) unique--;
+    marker[charCode]++;
+
+    let lastCarCode = line[i - len].charCodeAt(0);
+    if (marker[charCode] === 2) unique++;
+    if (marker[charCode] === 1) unique--;
+    marker[lastCarCode]--;
+
+    if (unique === len) return i + 1;
+  }
+}
