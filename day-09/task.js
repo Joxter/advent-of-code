78ee447b7ced6dc8029f9b205ed4034ede76cbd9
Joxter <joxterrr@gmail.com>
2022-12-09 18:22:52 +0100

    day 9, part 1 (correct approach)

--- day-09/task.js
+++ day-09/task.js
@@ -8,6 +8,9 @@ let inputData = fs.readFileSync('./input.txt').toString();
 console.log('test OK: ', part1(testInput) === 13);
 console.log('answer: ', part1(inputData));
 
+console.log('test_actual OK: ', part1_correct_approach_actually(testInput, 1) === 13);
+console.log('answer_actual: ', part1_correct_approach_actually(inputData, 1));
+
 // console.log('test2 OK: ', part2(testInput) === 8);
 // console.log('answer2: ', part2(inputData));
 
@@ -22,7 +25,7 @@ function part1(inp) {
   };
 
   let history = new Set();
-  history.add(tail.join(','))
+  history.add(tail.join(','));
 
   inp.split('\n').map((line) => {
     let [direction, steps] = line.split(' ');
@@ -43,10 +46,64 @@ function part1(inp) {
   return history.size;
 }
 
+function part1_correct_approach_actually(inp, ropeLen) {
+  let history = new Set();
+  let rope = Array.from({length: ropeLen + 1}, () => {
+    return [0, 0];
+  });
+  let delta = {'U': [1, 0], 'D': [-1, 0], 'R': [0, 1], 'L': [0, -1],};
+
+  inp.split('\n').map((line) => {
+    let [direction, steps] = line.split(' ');
+    steps = +steps;
+
+    while (steps--) {
+      rope[0][0] += delta[direction][0];
+      rope[0][1] += delta[direction][1];
+
+      for (let i = 1; i < rope.length; i++) {
+        rope[i] = follow(rope[i], rope[i - 1]);
+      }
+      // console.log(rope[ropeLen]);
+      history.add(rope[ropeLen].join(','));
+    }
+  });
+
+  return history.size;
+}
+
 function part2(inp) {
   // WTF???
 }
 
 function lengthBetween(a, b) {
   return Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]));
+}
+
+function follow(a, b) {
+  let res = [...a];
+
+  if (a[0] + 2 === b[0]) {
+    res[0]++;
+    if (a[1] + 1 === b[1]) res[1]++;
+    if (a[1] - 1 === b[1]) res[1]--;
+    return res;
+  } else if (a[0] - 2 === b[0]) {
+    res[0]--;
+    if (a[1] + 1 === b[1]) res[1]++;
+    if (a[1] - 1 === b[1]) res[1]--;
+    return res;
+  } else if (a[1] + 2 === b[1]) {
+    res[1]++;
+    if (a[0] + 1 === b[0]) res[0]++;
+    if (a[0] - 1 === b[0]) res[0]--;
+    return res;
+  } else if (a[1] - 2 === b[1]) {
+    res[1]--;
+    if (a[0] + 1 === b[0]) res[0]++;
+    if (a[0] - 1 === b[0]) res[0]--;
+    return res;
+  }
+
+  return res;
 }
