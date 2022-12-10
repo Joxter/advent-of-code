0a2a2cc035785c70dabca19c01ec6d42f99df12c
Joxter <joxterrr@gmail.com>
2022-12-10 10:07:52 +0100

    day 10, part 2

--- day-10/task.js
+++ day-10/task.js
@@ -5,14 +5,22 @@ import fs from 'fs';
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
 
-// fs.writeFileSync('./testDataALT.txt', testInput.replaceAll('addx ', 'noop\naddx '));
-// fs.writeFileSync('./inputALT.txt', inputData.replaceAll('addx ', 'noop\naddx '));
-
 console.log('test OK: ', part1(testInput) === 13140);
 console.log('answer: ', part1(inputData));
 
-// console.log('test2 OK: ', part2(test2Input) === 36);
-// console.log('answer2: ', part2(inputData), 2619); // 491 is wrong???? correct 2619 O_o
+let res = part2(testInput);
+console.log('test2:\n' + res);
+res = part2(inputData);
+console.log('answer2:\n' + res);
+// B J F R H R F U
+/*
+###....##.####.###..#..#.###..####.#..#.
+#..#....#.#....#..#.#..#.#..#.#....#..#.
+###.....#.###..#..#.####.#..#.###..#..#.
+#..#....#.#....###..#..#.###..#....#..#.
+#..#.#..#.#....#.#..#..#.#.#..#....#..#.
+###...##..#....#..#.#..#.#..#.#.....##..
+*/
 
 function part1(inp) {
   let commands = [];
@@ -51,5 +59,63 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  //
+  let commands = [];
+  let result = '';
+  inp.split('\n').map((line) => {
+    let [command, value] = line.split(' ');
+    if (command === 'addx') {
+      commands.push(0, +value);
+    } else {
+      commands.push(0);
+    }
+  });
+
+  let xValue = 1;
+  let i = 0;
+  for (let j = 0; j < 40; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  result += '\n';
+  xValue += 40;
+
+  for (let j = 40; j < 80; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  result += '\n';
+  xValue += 40;
+
+  for (let j = 80; j < 120; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  result += '\n';
+  xValue += 40;
+
+  for (let j = 120; j < 160; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  result += '\n';
+  xValue += 40;
+
+  for (let j = 160; j < 200; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  result += '\n';
+  xValue += 40;
+
+  for (let j = 200; j < 240; j++) {
+    result += j - 1 <= xValue && j + 1 >= xValue ? '#' : '.';
+    xValue += commands[i++];
+  }
+
+  return result;
 }
