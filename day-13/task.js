c58b677de003c66e4bc4830c6f9ec992c1b70d5e
Joxter <joxterrr@gmail.com>
2022-12-13 09:09:58 +0100

    Day 13 clean up

--- day-13/task.js
+++ day-13/task.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 
-// https://adventofcode.com/2022/day/12
+// https://adventofcode.com/2022/day/13
 
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
 
-// console.log('test OK: ', part1(testInput) === 13);
-// console.log('answer: ', part1(inputData));
+console.log('test OK: ', part1(testInput) === 13);
+console.log('answer: ', part1(inputData));
 
 console.log('test2 OK:', part2(testInput) === 140);
 console.log('answer2:', part2(inputData));
@@ -18,8 +18,7 @@ function part1(inp) {
     let right = JSON.parse(_right);
 
     let isCorrect = compare(left, right);
-    // console.log(i + 1, isCorrect ? i + 1 : 0);
-    return isCorrect ? i + 1 : 0;
+    return isCorrect === 1 ? i + 1 : 0;
   }).reduce((sum, n) => sum + n, 0);
 }
 
@@ -28,15 +27,11 @@ function part2(inp) {
     let [_left, _right] = blocks.split('\n');
     let left = JSON.parse(_left);
     let right = JSON.parse(_right);
-    return [left, right]
-  }).flat()
-  arr.push([[2]], [[6]])
+    return [left, right];
+  }).flat();
 
-  console.log(arr);
-
-  arr.sort(compare).reverse()
-
-  console.log(arr);
+  arr.push([[2]], [[6]]);
+  arr.sort(compare).reverse();
 
   let indexOf2 = arr.findIndex((it) => it.length === 1 && it[0].length === 1 && it[0][0] === 2) + 1;
   let indexOf6 = arr.findIndex((it) => it.length === 1 && it[0].length === 1 && it[0][0] === 6) + 1;
@@ -45,44 +40,27 @@ function part2(inp) {
 }
 
 function compare(left, right) {
-  left = JSON.parse(JSON.stringify(left))
-  right = JSON.parse(JSON.stringify(right))
-  // console.log(inp);
+  left = [...left];
+  right = [...right];
 
   while (left.length > 0 && right.length > 0) {
     let l = left.shift();
     let r = right.shift();
 
-    if (typeof l !== 'number' || typeof r !== 'number') {
+    if (Array.isArray(l) || Array.isArray(r)) {
       let res = compare(
         Array.isArray(l) ? l : [l],
         Array.isArray(r) ? r : [r]
       );
 
-      if (res !== 0) {
-        // console.log(inp, res);
-        return res;
-      }
+      if (res !== 0) return res;
     }
 
-    if (l > r) {
-      // console.log(inp, false);
-      return -1;
-    }
-    if (l < r) {
-      // console.log(inp, true);
-      return 1;
-    }
+    if (l > r) return -1;
+    if (l < r) return 1;
   }
 
-  if (left.length < right.length) {
-    // console.log(inp, true);
-    return 2;
-  }
-  if (left.length > right.length) {
-    // console.log(inp, false);
-    return -1;
-  }
-  // console.log(inp, null);
+  if (left.length < right.length) return 1;
+  if (left.length > right.length) return -1;
   return 0;
 }
