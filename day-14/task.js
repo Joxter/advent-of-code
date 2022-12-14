4e4d910a39df8c3cac3d8b4b7203868f45742d0d
Nikolay Morozov <joxterrr@gmail.com>
2022-12-14 13:08:20 +0100

    Day 14, done

--- day-14/task.js
+++ day-14/task.js
@@ -1,27 +1,31 @@
-import fs from 'fs';
+import fs from "fs";
 
 // https://adventofcode.com/2022/day/14
 
-let testInput = fs.readFileSync('./testData.txt').toString();
-let inputData = fs.readFileSync('./input.txt').toString();
+let testInput = fs.readFileSync("./testData.txt").toString();
+let inputData = fs.readFileSync("./input.txt").toString();
 
-console.log('test OK: ', part1(testInput) === 24);
-// console.log('answer: ', part1(inputData));
+console.log("test OK: ", part1(testInput) === 24);
+console.log("answer: ", part1(inputData), [745]);
 
-// console.log('test2 OK:', part2(testInput) === 140);
-// console.log('answer2:', part2(inputData));
+console.log("test2 OK:", part2(testInput) === 93);
+console.log("answer2:", part2(inputData), [27551]);
 
 function part1(inp) {
-  let map = {}; // [x-y]: #/o
-  inp.split('\n').forEach((line) => {
-    console.log(line);
-    let [[hx, hy], ...target] = line.split(` -> `).map((xy) => xy.split(',').map(n => +n));
+  let map = {};
+
+  let lastLeftRock = 500;
+  let lastRightRock = 500;
+  let bottom = 0;
+
+  inp.split("\n").forEach((line) => {
+    let [[hx, hy], ...target] = line
+      .split(` -> `)
+      .map((xy) => xy.split(",").map((n) => +n));
+    map[hx + "," + hy] = "#";
 
     target.forEach(([tx, ty]) => {
-      let [tx, ty] = t;
-
-      let i = 1000;
-      while ((tx !== hx || ty !== hy) && i--) {
+      while (tx !== hx || ty !== hy) {
         if (tx === hx && ty > hy) {
           hy++;
         } else if (tx === hx && ty < hy) {
@@ -31,29 +35,123 @@ function part1(inp) {
         } else if (ty === hy && tx < hx) {
           hx--;
         }
-        map[hx + ',' + hy] = '#';
-      }
-      if (i < 1) throw new Error('OVERFLOW')
-    });
+        map[hx + "," + hy] = "#";
 
+        if (hx < lastLeftRock) lastLeftRock = hx;
+        if (hx > lastRightRock) lastRightRock = hx;
+        bottom = Math.max(bottom, hy);
+      }
+    });
   });
 
-  console.log(map);
+  function getFinishPos([x, y]) {
+    if (y > bottom) return null;
 
-  /*
-  498,4 -> 498,6 -> 496,6
-  503,4 -> 502,4 -> 502,9 -> 494,9
-  */
+    if (!map[x + "," + (y + 1)]) {
+      return getFinishPos([x, y + 1]);
+    }
+    if (!map[x - 1 + "," + (y + 1)]) {
+      return getFinishPos([x - 1, y + 1]);
+    }
+    if (!map[x + 1 + "," + (y + 1)]) {
+      return getFinishPos([x + 1, y + 1]);
+    }
 
+    return [x, y];
+  }
 
   let result = 0;
-  let lastLeftRock = 500;
-  let lastRightRock = 500;
   let sandSource = [500, 0];
 
+  while (true) {
+    let lastPose = getFinishPos(sandSource);
+    if (!lastPose) {
+      break;
+    }
+    map[lastPose.join(",")] = "o";
+    result++;
+  }
+  // render(map, lastLeftRock, lastRightRock, bottom);
 
   return result;
 }
 
 function part2(inp) {
+  let map = {};
+
+  let lastLeftRock = 500;
+  let lastRightRock = 500;
+  let bottom = 0;
+
+  inp.split("\n").forEach((line) => {
+    let [[hx, hy], ...target] = line
+      .split(` -> `)
+      .map((xy) => xy.split(",").map((n) => +n));
+    map[hx + "," + hy] = "#";
+
+    target.forEach(([tx, ty]) => {
+      while (tx !== hx || ty !== hy) {
+        if (tx === hx && ty > hy) {
+          hy++;
+        } else if (tx === hx && ty < hy) {
+          hy--;
+        } else if (ty === hy && tx > hx) {
+          hx++;
+        } else if (ty === hy && tx < hx) {
+          hx--;
+        }
+        map[hx + "," + hy] = "#";
+
+        if (hx < lastLeftRock) lastLeftRock = hx;
+        if (hx > lastRightRock) lastRightRock = hx;
+        bottom = Math.max(bottom, hy);
+      }
+    });
+  });
+
+  function getFinishPos([x, y]) {
+    if (y > bottom) return [x, y];
+
+    if (!map[x + "," + (y + 1)]) return getFinishPos([x, y + 1]);
+    if (!map[x - 1 + "," + (y + 1)]) return getFinishPos([x - 1, y + 1]);
+    if (!map[x + 1 + "," + (y + 1)]) return getFinishPos([x + 1, y + 1]);
+
+    return [x, y];
+  }
+
+  let result = 0;
+  let sandSource = [500, 0];
+
+  while (true) {
+    let lastPose = getFinishPos(sandSource);
+
+    if (lastPose[0] === sandSource[0] && lastPose[1] === sandSource[1]) {
+      map[lastPose.join(",")] = "o";
+      result++;
+      break;
+    }
+
+    map[lastPose.join(",")] = "o";
+    if (lastPose[0] < lastLeftRock) lastLeftRock = lastPose[0];
+    if (lastPose[0] > lastRightRock) lastRightRock = lastPose[0];
+
+    result++;
+  }
+  // render(map, lastLeftRock, lastRightRock, bottom);
+
+  return result;
+}
+
+function render(map, lastLeftRock, lastRightRock, bottom) {
+  let result = "";
+
+  for (let y = -1; y <= bottom + 2; y++) {
+    let row = "";
+    for (let x = lastLeftRock - 2; x < lastRightRock + 2; x++) {
+      row += map[x + "," + y] || ".";
+    }
+    result += String(y).padStart(3, "0") + " " + row + "\n";
+  }
+
+  console.log(result);
 }
