5ff0ef7d5e9e3d53007fc339cbf1d9e64e1512aa
Joxter <joxterrr@gmail.com>
2022-12-17 11:48:49 +0100

    add template folder, rename some days

---
+++ day-00/task.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+// https://adventofcode.com/2022/day/**********
+
+let testInput = fs.readFileSync('./testData.txt').toString();
+let inputData = fs.readFileSync('./input.txt').toString();
+
+console.log('test OK: ', part1(testInput) === 123);
+console.log('answer: ', part1(inputData));
+
+// console.log('test2 OK:', part1(testInput) === 123);
+// console.log('answer2:', part1(inputData));
+
+function part1(inp) {
+  let result = 0;
+  inp.trim().split('\n').forEach((line) => {
+  });
+  return result
+}
+
+function part2(inp) {
+  let result = 0;
+  inp.trim().split('\n').forEach((line) => {
+  });
+  return result
+}
