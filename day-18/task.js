63523a660d7c00a17e361324acf7a31a46532292
Joxter <joxterrr@gmail.com>
2022-12-18 09:36:39 +0100

    Day 18, clean up

--- day-18/task.js
+++ day-18/task.js
@@ -6,60 +6,66 @@ let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
 
 console.log('test OK: ', part1(testInput) === 64);
-console.log('answer: ', part1(inputData));
+console.log('answer: ', part1(inputData), [4580]);
 
-// console.log('test2 OK:', part1(testInput) === 123);
-// console.log('answer2:', part1(inputData));
+// console.log('test2 OK:', part2(testInput), [58]);
+// console.log('answer2:', part2(inputData));
 
 function part1(inp) {
-  let result = 0;
+  let map = {};
+  inp.trim().split('\n').forEach((line) => {
+    map[line] = true;
+  });
+
+  return countSurface(map)
+}
+
+function countSurface(map) {
+  return Object.keys(map).map((position) => {
+    let [x, y, z] = position.split(',').map(it => +it);
+    return 6 - getCubesAround(map, [x,y,z]);
+  }).reduce((sum, it) => sum + it, 0);
+}
+
+function part2(inp) {
   let map = {};
 
   inp.trim().split('\n').forEach((line) => {
     map[line] = true;
   });
 
-  console.log(map);
+  let lava = {};
 
-  return Object.keys(map).map((position) => {
-    let [x, y, z] = position.split(',').map(it => +it);
+  // fill in with lava
+  // count where lava touches bricks
 
-    let open = 6;
-
-    if (map[[x + 1, y, z].join(',')]) {
-      // console.log('> 1');
-      open--;
-    }
-    if (map[[x, y + 1, z].join(',')]) {
-      // console.log('> 2');
-      open--;
-    }
-    if (map[[x, y, z + 1].join(',')]) {
-      // console.log('> 3');
-      open--;
-    }
-
-    if (map[[x - 1, y, z].join(',')]) {
-      // console.log('> 4');
-      open--;
-    }
-    if (map[[x, y - 1, z].join(',')]) {
-      // console.log('> 5');
-      open--;
-    }
-    if (map[[x, y, z - 1].join(',')]) {
-      // console.log('> 6');
-      open--;
-    }
-
-    // console.log(open);
-    return open;
-  }).reduce((sum, it) => sum + it, 0);
 }
+function getCubesAround(map, [x, y, z]) {
+  let cnt = 0;
+  if (map[[x + 1, y, z].join(',')]) {
+    cnt++;
+  }
+  if (map[[x, y + 1, z].join(',')]) {
+    // console.log('> 2');
+    cnt++;
+  }
+  if (map[[x, y, z + 1].join(',')]) {
+    // console.log('> 3');
+    cnt++;
+  }
 
-function part2(inp) {
-  let result = 0;
-  inp.trim().split('\n').forEach((line) => {
-  });
-  return result;
+  if (map[[x - 1, y, z].join(',')]) {
+    // console.log('> 4');
+    cnt++;
+  }
+  if (map[[x, y - 1, z].join(',')]) {
+    // console.log('> 5');
+    cnt++;
+  }
+  if (map[[x, y, z - 1].join(',')]) {
+    // console.log('> 6');
+    cnt++;
+  }
+
+  return cnt;
 }
