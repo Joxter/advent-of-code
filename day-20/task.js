2fe20d9ee0b5651cec49f8cca1227255f8f8d996
Joxter <joxterrr@gmail.com>
2022-12-20 14:18:17 +0100

    Day 20, part2, clean up

--- day-20/task.js
+++ day-20/task.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-// https://adventofcode.com/2022/day/     20
+// https://adventofcode.com/2022/day/20
 
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
@@ -8,11 +8,6 @@ let inputData = fs.readFileSync('./input.txt').toString();
 // for (let i = 0; i < 100; i++) {
 //   test(`-1, -2, ${i}, -3, -4, -5, -6`, 2);
 // }
-
-// test('01, 2, -51, 3, 4, 5, 6', 2);
-// test('1, 2, -52, 3, 4, 5, 6', 2);
-// test('1, 2, -1, 3, 4, 5, 6', 2);
-
 // for (let i = 0; i < 100; i++) {
 //   test(`0, 1, 2, -${i}, 3, 4, 5, 6`, 3);
 // }
@@ -20,9 +15,8 @@ let inputData = fs.readFileSync('./input.txt').toString();
 console.log('test OK: ', part1(testInput) === 3);
 console.log('answer: ', part1(inputData), [7278]);
 
-// console.log('test2 OK:', part2(testInput) === 1623178306);
-// console.log('answer2:', part2(inputData));
-
+console.log('test2 OK:', part2(testInput) === 1623178306);
+console.log('answer2:', part2(inputData), [14375678667089]);
 
 function part1(inp) {
   let arr = [];
@@ -31,7 +25,7 @@ function part1(inp) {
   });
 
   for (let i = 0; i < arr.length; i++) {
-    precess2(arr, i);
+    mix(arr, i);
   }
 
   let zeroIndex = arr.findIndex((it) => it.val === 0);
@@ -46,92 +40,22 @@ function part1(inp) {
 function part2(inp) {
   let arr = [];
   inp.trim().split('\n').forEach((line, i) => {
-    arr.push({val: +line, original: i});
+    arr.push({val: +line * 811589153, original: i});
   });
 
+  for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      mix(arr, i);
+    }
+  }
 
-  // if (i === 2) break;
+  let zeroIndex = arr.findIndex((it) => it.val === 0);
 
-  /*
-  0  1  2  3  4  5   6
-       {-4}        !
-      arr.len7: from 2 to -4 (eq +3)
+  let a = arr[(zeroIndex + 1000) % arr.length].val;
+  let b = arr[(zeroIndex + 2000) % arr.length].val;
+  let c = arr[(zeroIndex + 3000) % arr.length].val;
 
-  0  1  2  3  4  5   6
-      !      {-9}
-      arr.len7: from 4 to -9 (eq -2)
-
-
-=======================================
-=======================================
-=======================================
-=======================================
-  0  1  2  3  4  5  6
-         {+3}
-  0  1  2  4  5  6  3
-  arr.len7: from 3 to +3.  REMOVE 3, INSERT IN 6 (remove baseIndex, insert baseIndex + +3)
-=======================================
-  0  1  2  3  4  5  6
-         {+2}
-  0  1  2  4  5  3  6
-  arr.len7: from 3 to +2.  REMOVE 3, INSERT IN 5 (remove baseIndex, insert baseIndex + +2)
-
-=======================================
-  0  1  2  3  4  5  6
-         {+8}
-  0  1  2  4  3  5  6
-  arr.len7: from 3 to +2.  REMOVE 3, INSERT IN 4 (remove baseIndex, insert (baseIndex + +8) % arr.len)
-
-=======================================
-  0  1  2  3  4  5  6
-         {+5}
-  0  1  3  2  4  5  6
-  arr.len7: from 3 to +2.  REMOVE 3, INSERT IN 2 (remove baseIndex, insert ((baseIndex + +5) % arr.len) + 1)
-
-=======================================
-  0  1  2  3  4  5  6
-         {-2}
-  0  3  1  2  4  5  6
-  arr.len7: from 3 to +2.  REMOVE 3, INSERT IN 1 (remove baseIndex, insert baseIndex + -2)
-
-=======================================
-  0  1  2  3  4  5  6
-         {+1}
-  0  1  2  4  3  5  6
-  arr.len7: from 3 to +2.  REMOVE 3, INSERT IN 4 (remove baseIndex, insert baseIndex + -2)
-
-  0  1  2  3  4  5  6
-                   {+1}
-  6  0  1  2  3  4  5
-  arr.len7: from 3 to +2.  REMOVE 6, INSERT IN 0 (remove baseIndex, insert baseIndex + -2)
-
-
-  */
-
-  /*
-      if (target.val > 0) {
-
-        // if (targetIndex + target.val <= arr.length) {
-          arr.splice(targetIndex + target.val + 1, 0, target);
-          arr.splice(targetIndex, 1);
-          continue;
-        // }
-      }
-
-      if (target.val > 0 && targetIndex + target.val > arr.length) {
-        let newPlace = targetIndex + target.val;
-        arr.splice(targetIndex + target.val + 1, 0, target);
-        arr.splice(targetIndex, 1);
-        continue;
-      }
-      //
-      if (target.val < 0 && targetIndex + target.val < 0) {
-        arr.splice(arr.length - (-target.val - targetIndex), 0, target);
-        arr.splice(targetIndex, 1);
-      }
-  */
-
-  return result;
+  return a + b + c;
 }
 
 function test(input, ind) {
@@ -139,8 +63,8 @@ function test(input, ind) {
     return {val: +val, original: i};
   });
 
-  let expected = precess([...arr], ind).map((it) => it.val).join(', ');
-  let actual = precess2([...arr], ind).map((it) => it.val).join(', ');
+  let expected = mix_OLD([...arr], ind).map((it) => it.val).join(', ');
+  let actual = mix([...arr], ind).map((it) => it.val).join(', ');
   if (expected === actual) {
     console.log(`OK! ${actual}`);
   } else {
@@ -152,42 +76,33 @@ function test(input, ind) {
   }
 }
 
-function precess2(arr, original) {
+function mix(arr, original) {
   let targetIndex = arr.findIndex((it) => it.original === original);
   let target = arr[targetIndex];
 
   if (target.val === 0) return arr;
 
+  arr.splice(targetIndex, 1);
+
   if (target.val > 0) {
-    arr.splice(targetIndex, 1);
-    let offset1 = arr.length - targetIndex;
-    let newIndex = (target.val - offset1) % arr.length;
-    // console.log('offset: ', offset1);
-    // console.log('offset: ', (arr.length - targetIndex) % arr.length);
-    // console.log(arr.length - targetIndex);
-    // console.log({newIndex});
+    let firstLineOffset = arr.length - targetIndex;
+    let newIndex = (target.val - firstLineOffset) % arr.length;
     arr.splice(newIndex, 0, target);
+
   } else {
-    arr.splice(targetIndex, 1);
+    let firstLineOffset = targetIndex;
 
-    let offset1 = targetIndex;
-    if (-target.val < offset1) {
-      arr.splice(offset1 - -target.val, 0, target);
-    } else {
+    let newIndex = -target.val < firstLineOffset
+      ? firstLineOffset - -target.val
+      : arr.length - (-target.val - firstLineOffset) % arr.length;
 
-      let newIndex = Math.abs(-target.val - offset1) % arr.length;
-      // console.log('offset: ', offset1);
-      // console.log('offset: ', (arr.length - targetIndex) % arr.length);
-      // console.log(arr.length - targetIndex);
-      // console.log({newIndex});
-      arr.splice(arr.length - newIndex, 0, target);
-    }
+    arr.splice(newIndex, 0, target);
   }
 
   return arr;
 }
 
-function precess(arr, original) {
+function mix_OLD(arr, original) {
   let targetIndex = arr.findIndex((it) => it.original === original);
   let target = arr[targetIndex];
 
