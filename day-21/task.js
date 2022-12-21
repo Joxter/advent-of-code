feea7cfc6e3a998cae4ed3b1d89214c260800d4b
Nikolay Morozov <joxterrr@gmail.com>
2022-12-21 10:33:27 +0100

    Day 21, part1

--- day-21/task.js
+++ day-21/task.js
@@ -5,74 +5,44 @@ import fs from 'fs';
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
 
-console.log('test OK: ', part1(testInput) === 152);
-// console.log('answer: ', part1(inputData));
+console.log("test OK: ", part1(testInput) === 152);
+console.log("answer: ", part1(getRealData())), [10037517593724];
 
 // console.log('test2 OK:', part2(testInput) === 123);
 // console.log('answer2:', part2(inputData));
 
 function part1(inp) {
-  let result = 0;
-
-  let nums = new Map();
   let ops = new Map();
 
-  inp.trim().split('\n').forEach((line) => {
-    let [name, ..._op] = line.split(' ')
+  inp
+    .trim()
+    .split("\n")
+    .forEach((line) => {
+      let [name, _op] = line.split(": ");
 
-    if (+_op[0]) {
-      nums.set(name, +_op[0])
-    } else {
-      let [left, op, right] = _op
-      ops.set(name, [left, op, right])
-    }
-  });
-
-  /*
-  Map(8) {
-  'dbpl:' => 5,
-  'zczc:' => 2,
-  'dvpt:' => 3,
-  'lfqf:' => 4,
-  'humn:' => 5,
-  'ljgn:' => 2,
-  'sllz:' => 4,
-  'hmdt:' => 32
-  'root:' => [ 'pppw', '+', 'sjmn' ],
-  'cczh:' => [ 'sllz', '+', 'lgvd' ],
-  'ptdq:' => [ 'humn', '-', 'dvpt' ],
-  'sjmn:' => [ 'drzm', '*', 'dbpl' ],
-  'pppw:' => [ 'cczh', '/', 'lfqf' ],
-  'lgvd:' => [ 'ljgn', '*', 'ptdq' ],
-  'drzm:' => [ 'hmdt', '-', 'zczc' ]
-*/
-  console.log(nums, ops);
-  return ;
-
-  let i = 20
-  while (ops.size > 0 && i--) {
-    // console.log(1111);
-    for (let [name, [left, op, right]] of ops) {
-      console.log(left, op, right);
-      if (nums.has(left) && nums.has(right)) {
-        let res = {
-          '+': +left + +right,
-          '-': +left - +right,
-          '*': +left * +right,
-          '/': +left / +right,
-        }
-        nums.set(name, res[op])
-        ops.delete(name)
+      if (+_op) {
+        ops.set(name, { num: +_op });
+      } else {
+        let [left, op, right] = _op.split(" ");
+        ops.set(name, { left, op, right });
       }
-    }
-    console.log(ops.size);
+    });
 
-    // return
+  function calc(nodeName) {
+    let aaa = ops.get(nodeName);
+
+    if (aaa.num) {
+      return aaa.num;
+    } else {
+      let { left, op, right } = aaa;
+      if (op === "+") return calc(left) + calc(right);
+      if (op === "-") return calc(left) - calc(right);
+      if (op === "*") return calc(left) * calc(right);
+      if (op === "/") return calc(left) / calc(right);
+    }
   }
 
-
-
-  return nums.get('root')
+  return calc("root");
 }
 
 function part2(inp) {
