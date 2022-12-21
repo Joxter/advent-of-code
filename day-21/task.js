33031b1fe1f28c17268dfdbdaeb32633e351245e
Nikolay Morozov <joxterrr@gmail.com>
2022-12-21 11:42:50 +0100

    Day 21, part2 test ok

--- day-21/task.js
+++ day-21/task.js
@@ -46,8 +46,71 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  let result = 0;
-  inp.trim().split('\n').forEach((line) => {
-  });
-  return result
+  let ops = new Map();
+
+  inp
+    .trim()
+    .split("\n")
+    .forEach((line) => {
+      let [name, _op] = line.split(": ");
+
+      if (+_op) {
+        ops.set(name, { num: +_op });
+      } else {
+        let [left, op, right] = _op.split(" ");
+        ops.set(name, { left, op, right });
+      }
+    });
+  let myName = "humn";
+
+  // let leftRoot = calc(ops.get("root").left);
+  let rightRoot = calc(ops.get("root").right);
+
+  // answer2: 3886130271980.3667
+  // WTF ????
+  // 3886130271980 too high
+  return calcHuman(ops.get("root").left, rightRoot);
+
+  function calc(nodeName) {
+    if (nodeName === myName) return NaN;
+    let aaa = ops.get(nodeName);
+
+    if (aaa.num) {
+      return aaa.num;
+    } else {
+      let { left, op, right } = aaa;
+      if (op === "+") return calc(left) + calc(right);
+      if (op === "-") return calc(left) - calc(right);
+      if (op === "*") return calc(left) * calc(right);
+      if (op === "/") return calc(left) / calc(right);
+    }
+  }
+
+  function calcHuman(nodeName, target) {
+    if (nodeName === myName) return target;
+    let aaa = ops.get(nodeName);
+
+    if (aaa.num) {
+      return aaa.num;
+    } else {
+      let { left, op, right } = aaa;
+
+      let leftVal = calc(left);
+      let rightVal = calc(right);
+
+      if (Number.isNaN(leftVal)) {
+        if (op === "+") return calcHuman(left, target - rightVal);
+        if (op === "-") return calcHuman(left, target + rightVal);
+        if (op === "*") return calcHuman(left, target / rightVal);
+        if (op === "/") return calcHuman(left, target * rightVal);
+      }
+
+      if (Number.isNaN(rightVal)) {
+        if (op === "+") return calcHuman(right, target - leftVal);
+        if (op === "-") return calcHuman(right, target + leftVal);
+        if (op === "*") return calcHuman(right, target / leftVal);
+        if (op === "/") return calcHuman(right, target * leftVal);
+      }
+    }
+  }
 }
