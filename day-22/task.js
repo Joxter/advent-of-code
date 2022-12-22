747765e2151d7642676c5bf7976314c330051476
Joxter <joxterrr@gmail.com>
2022-12-22 17:47:49 +0100

    Day 22, part2 test works

--- day-22/task.js
+++ day-22/task.js
@@ -4,12 +4,15 @@ import fs from 'fs';
 
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
+let inputTEST = fs.readFileSync('./inputTEST.txt').toString();
 
-console.log('test OK: ', part1(testInput), [6032]);
-console.log('answer: ', part1(inputData));
+// console.log('test OK: ', part1(testInput), [6032]);
+// console.log('answer: ', part1(inputData));
 
 // console.log('test2 OK:', part2(testInput) === 123);
 // console.log('answer2:', part2(inputData));
+
+console.log('answer2:', part2(inputTEST));
 
 function part1(inp) {
   let dirDelta = {
@@ -54,7 +57,7 @@ function part1(inp) {
       path[curPos.join(',')] = directionArrow;
     }
   });
-  // render(map, path, curPos);
+  render(map, path, curPos);
 
   let arrowPoint = {'>': 0, 'v': 1, '<': 2, '^': 3}[directionArrow];
 
@@ -122,7 +125,6 @@ function part2(inp) {
   // row, col
   let curPos = [0, map[0].findIndex(it => it === '.')];
 
-  // 10R5L5R10L4R5L5
   let [curSteps, ...route] = strRoute.split(/(?=[LR]\d+)/);
   curSteps = +curSteps;
   route.unshift('R' + curSteps);
@@ -133,6 +135,10 @@ function part2(inp) {
 
   let directionArrow = clockDir[currDirection % 4];
 
+  route = [
+    'R51', 'R52', 'L3', 'L6', 'R47', 'L3', 'L6', 'R47', 'L3', 'L12', 'R102', 'R2', 'R12', 'L12', 'R4', 'R60',
+    'R10', 'L3', 'L7', 'R95', 'L3', 'L10'
+  ];
   route.forEach((p, i) => {
     let direction = p.slice(0, 1);
     let steps = +p.slice(1);
@@ -141,39 +147,53 @@ function part2(inp) {
     } else {
       currDirection--;
     }
-    directionArrow = clockDir[currDirection % 4];
+    // directionArrow = clockDir[currDirection % 4];
+    // console.log(directionArrow);
 
     for (let i = 0; i < steps; i++) {
-      curPos = getStepCoords(curPos, directionArrow);
+      [curPos, currDirection] = getStepCoords(curPos, clockDir[currDirection % 4]);
 
-      path[curPos.join(',')] = directionArrow;
+      path[curPos.join(',')] = clockDir[currDirection % 4];
     }
   });
+  render(map, path, curPos);
 
   let arrowPoint = {'>': 0, 'v': 1, '<': 2, '^': 3}[directionArrow];
 
   return 1000 * (curPos[0] + 1) + 4 * (curPos[1] + 1) + arrowPoint;
 
   function getStepCoords([row, col], directionArrow) {
+    let right = 100_000;
+    let down = 100_001;
+    let left = 100_002;
+    let up = 100_003;
+    let arrowToNumber = {'>': right, '<': left, '^': up, 'v': down};
+
     let delta = dirDelta[directionArrow];
 
-    if (map[row + delta[0]] && map[row + delta[0]][col + delta[1]]) {
-      let nextChar = map[row + delta[0]][col + delta[1]];
+    let newRow = row + delta[0];
+    let newCol = col + delta[1];
 
-      if (nextChar === '.') return [row + delta[0], col + delta[1]];
-      if (nextChar === '#') return [row, col];
+    if (map[newRow] && map[newRow][newCol]) {
+      let nextChar = map[newRow][newCol];
 
-      let cur = [row, col];
-      // todo moveTile()
-      return cur;
-    } else {
-      let cur = [row, col];
+      if (nextChar === '.') return [[newRow, newCol], arrowToNumber[directionArrow]];
+      if (nextChar === '#') return [[row, col], arrowToNumber[directionArrow]];
 
-      // todo moveTile()
-      return cur;
+      let [mCoords, dir] = moveTile([newRow, newCol], directionArrow);
+
+      if (map[mCoords[0]][mCoords[1]] === '#') {
+        return [[row, col], arrowToNumber[directionArrow]];
+      }
+      return [mCoords, dir];
     }
 
-    return [row, col];
+    let [mCoords, dir] = moveTile([newRow, newCol], directionArrow);
+
+    if (map[mCoords[0]][mCoords[1]] === '#') {
+      return [[row, col], arrowToNumber[directionArrow]];
+    }
+    return [mCoords, dir];
   }
 
   function moveTile([row, col], arrow) {
@@ -181,6 +201,8 @@ function part2(inp) {
     let down = 100_001;
     let left = 100_002;
     let up = 100_003;
+
+    console.log({row, col});
 
     let isLine1 = (it) => it >= 0 && it <= 49;
     let isLine2 = (it) => it >= 50 && it <= 99;
@@ -192,24 +214,66 @@ function part2(inp) {
 
     // line1, under 2
     if (arrow === 'v' && row === 50 && isLine3(col)) {
-      return [[col - 50, 99], right];
+      return [[col - 50, 99], left]; // OK
     }
     // line1, right 3
     if (arrow === '>' && col === 100 && isLine2(row)) {
-      return [[49, col + 50], up];
+      return [[49, row + 50], up]; // OK
     }
 
     // line2, right 2
     if (col === 150 && isLine1(row)) {
-      // 49 -> 100; 48 -> 101; 0  -> 149
-      return [[100 + (49 - row), 99], left];
+      return [[100 + (49 - row), 99], left]; // OK
     }
     // line2, right 4
     if (col === 100 && isLine3(row)) {
-      // 100 -> 49; 101 -> 48; 149 -> 0
-      return [[0 + (149 - row), 149], left];
+      return [[0 + (149 - row), 149], left]; // OK
     }
-    // todo make another 5 lines.. (line 3 is similar 1)
+
+    // line3, under 4
+    if (arrow === 'v' && row === 150 && isLine2(col)) {
+      return [[col + 100, 49], left]; // OK
+    }
+    // line3, right 6
+    if (arrow === '>' && col === 50 && isLine4(row)) {
+      return [[149, row - 100], up]; // OK
+    }
+
+    // line4, over 2
+    if (row === -1 && isLine3(col)) {
+      return [[199, col - 100], up]; // OK
+    }
+    // line4, under 6
+    if (row === 200 && isLine1(col)) {
+      return [[0, col + 100], down]; // OK
+    }
+
+    // line5, left 1
+    if (col === 49 && isLine1(row)) {
+      return [[150 - row, 0], right]; // OK
+    }
+    // line5, left 5
+    if (col === -1 && isLine3(row)) {
+      return [[150 - row, 50], right];// OK
+    }
+
+    // // line6, over 1
+    if (row === -1 && isLine2(col)) {
+      return [[100 + col, 0], right]; // OK
+    }
+    // // line6, left 6
+    if (col === -1 && isLine4(row)) {
+      return [[0, row - 100], down];
+    }
+
+    // line7, right 3
+    if (arrow === '<' && col === 49 && isLine2(row)) {
+      return [[100, row - 50], down]; // OK
+    }
+    // line7, up 5
+    if (arrow === '^' && row === 99 && isLine1(col)) {
+      return [[col + 50, 50], right]; // OK
+    }
 
     // [row, col]
     // 0-49 | 50-99 | 100-149 | 150-199 | 20
@@ -217,7 +281,7 @@ function part2(inp) {
           '>' '<'  '^'  'v'
     */
 
-    return [];
+    return [[row, col], down];
   }
 }
 
@@ -233,6 +297,6 @@ function render(map, path, final) {
 
     }).join('');
   }).join('\n');
-  console.log(result);
+  // console.log(result);
   fs.writeFileSync('./path.txt', result);
 }
