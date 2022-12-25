55e7512ba915f6a40e24c659de492e0b81d29adf
Joxter <joxterrr@gmail.com>
2022-12-25 15:58:19 +0100

    Day 25, done

---
+++ day-25/task.js
@@ -0,0 +1,106 @@
+import fs from 'fs';
+
+// https://adventofcode.com/2022/day/25
+
+let testInput = fs.readFileSync('./testData.txt').toString();
+let inputData = fs.readFileSync('./input.txt').toString();
+
+console.log('test OK: ', part1(testInput) === '2=-1=0');
+console.log('answer: ', part1(inputData) === '2-2=12=1-=-1=000=222' );
+
+function sum(a, b) {
+  a = a.split('').reverse();
+  b = b.split('').reverse();
+
+  let res = [];
+  let rem = '0';
+
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    let charA = a[i] || '0';
+    let charB = b[i] || '0';
+    let [newRem, ss] = sumAB(charA, charB);
+
+    let [newRem2, ss2] = sumAB(ss, rem);
+
+    res.push(ss2);
+    rem = sumAB(newRem, newRem2)[1];
+  }
+  if (rem !== '0') {
+    res.push(rem);
+  }
+  return res.reverse().join('');
+
+  function sumAB(a, b) {
+    let s = {
+      '==': ['-', '1'],
+      '=-': ['-', '2'],
+      '=0': ['0', '='],
+      '=1': ['0', '-'],
+      '=2': ['0', '0'],
+
+      '-=': ['-', '2'],
+      '--': ['0', '='],
+      '-0': ['0', '-'],
+      '-1': ['0', '0'],
+      '-2': ['0', '1'],
+
+      '0=': ['0', '='],
+      '0-': ['0', '-'],
+      '00': ['0', '0'],
+      '01': ['0', '1'],
+      '02': ['0', '2'],
+
+      '1=': ['0', '-'],
+      '1-': ['0', '0'],
+      '10': ['0', '1'],
+      '11': ['0', '2'],
+      '12': ['1', '='],
+
+      '2=': ['0', '0'],
+      '2-': ['0', '1'],
+      '20': ['0', '2'],
+      '21': ['1', '='],
+      '22': ['1', '-'],
+    };
+    return s[`${a}${b}`];
+  }
+}
+
+function part1(inp) {
+  let res = '0';
+  inp.trim().split('\n').forEach((line) => {
+    res = sum(res, line);
+  });
+  return res;
+}
+
+function fiveToSnafu(n) {
+  let dts = ['=', '-', '0', '1', '2'];
+
+  return n.split('').map((d) => dts[d]).join('');
+}
+
+function snafuToDec(snaf) {
+  let res = 0;
+  let std = {
+    '2': 2,
+    '1': 1,
+    '0': 0,
+    '-': -1,
+    '=': -2
+  };
+  let base = 1;
+
+  snaf.split('').reverse().forEach((s) => {
+    res = res + base * std[s];
+    base *= 5;
+  });
+  return res;
+}
+
+function part2(inp) {
+  let result = 0;
+  inp.trim().split('\n').forEach((line) => {
+  });
+  return result;
+}
