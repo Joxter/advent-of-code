19caa8f4a29e1292e06ca7e71098293fe4da669c
Joxter <joxterrr@gmail.com>
2023-01-14 20:01:46 +0100

    2022/16 done (todo fix perf)

--- day-16/task.js
+++ day-16/task.js
@@ -4,13 +4,23 @@ import fs from 'fs';
 
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
-// todo fix cash, without it p2 is incredibly slow
+// todo fix cash, p2 takes 10 mins
+//          upd. cash is huge and make it worse
 
+let start = Date.now();
 console.log('test OK: ', part1(testInput), [1651]);
 console.log('answer: ', part1(inputData), [2250]);
+console.log('sec', (Date.now() - start) / 1000);
 
+start = Date.now();
 console.log('test2 OK:', part2(testInput), [1707]);
 console.log('answer2:', part2(inputData), [3015]);
+console.log('sec', (Date.now() - start) / 1000);
+
+start = Date.now();
+console.log('(cash) test2 OK:', part2_cash(testInput), [1707]);
+console.log('(cash) answer2:', part2_cash(inputData), [3015]);
+console.log('sec', (Date.now() - start) / 1000);
 
 function parse(inp) {
   let map = {};
@@ -87,12 +97,11 @@ function part2(inp) {
 
   let { allOpened, heads, newNewMap } = parse(inp);
   let res = 0;
-  // let cash = {};
 
   let max = Math.floor(allOpened / 2);
 
   for (let i = 0; i < max; i++) {
-    console.log(i, max);
+    // console.log(i, max);
     let opened1 = i | heads['AA'];
 
     let res1 = dfs(newNewMap.AA, { released: 0, opened: opened1 }, 1);
@@ -104,10 +113,6 @@ function part2(inp) {
   return res;
 
   function dfs(node, { released, opened }, minutes) {
-    // let key = `${node.name},${opened},${minutes}`;
-    // if (key in cash) {
-    //   return cash[key];
-    // }
     if (opened === allOpened || minutes > maxMins) {
       return released;
     }
@@ -134,9 +139,63 @@ function part2(inp) {
           );
         })
     );
-    // cash[key] = result;
 
     return result;
+  }
+}
+
+function part2_cash(inp) {
+  let { allOpened, heads, newNewMap } = parse(inp);
+  let res = 0;
+  let cash = {};
+
+  let max = Math.floor(allOpened / 2);
+
+  for (let i = 0; i < max; i++) {
+    if (i % 10 === 0) {
+      // console.log(i, max, Object.keys(cash).length);
+    }
+    let opened1 = i | heads['AA'];
+
+    let res1 = dfs(newNewMap.AA, { released: 0, opened: opened1 }, 26);
+    let res2 = dfs(newNewMap.AA, { released: 0, opened: opened1 ^ allOpened }, 26);
+
+    res = Math.max(res, res1 + res2);
+  }
+
+  return res;
+
+  function dfs(node, { released, opened }, minutes) {
+    let key = `${node.name},${opened},${minutes}`;
+    if (key in cash) {
+      return cash[key];
+    }
+
+    let result = Math.max(
+      ...node.next
+        .map(_nodeName => {
+          let [nodeName, cost] = _nodeName.split(',');
+
+          let rate = +newNewMap[nodeName].rate;
+          let remTime = minutes - +cost - 1;
+
+          if (minutes <= 0 || (opened & heads[nodeName]) !== 0) {
+            return 0;
+          }
+
+          return dfs(
+            newNewMap[nodeName],
+            {
+              released: rate * remTime,
+              opened: opened | heads[nodeName],
+            },
+            remTime
+          );
+        })
+    );
+    cash[key] = released + result;
+
+    return released + result;
   }
 }
 
