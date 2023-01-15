d30bc159e04e3c2aa66f8caa9f6d4672573eb76f
Joxter <joxterrr@gmail.com>
2023-01-15 15:56:06 +0100

    2022/19 p2 wip, add cash

--- day-19_not_solved/task.js
+++ day-19_not_solved/task.js
@@ -6,11 +6,12 @@ import { runSolution } from '../utils.js';
 let testInput = fs.readFileSync('./testData.txt').toString();
 let inputData = fs.readFileSync('./input.txt').toString();
 
-// runSolution('test  ', () => part1(testInput), 33);  // sec 24
-// runSolution('part_1', () => part1(inputData), 2193) // sec 157
+//                                                                  cash
+// runSolution('test  ', () => part1(testInput), 33);  // sec 24     4.2
+// runSolution('part_1', () => part1(inputData), 2193) // sec 157   32.4
 
 runSolution('test  ', () => part2(testInput), 56);
-// runSolution('part_2', () => part2(inputData));
+runSolution('part_2', () => part2(inputData));
 
 function parse(inp) {
   let blueprints = inp.split('\n').map((line) => {
@@ -80,6 +81,7 @@ function simulateBlueprint(blueprint, maxMinutes) {
 
   let maxGeo = 0;
   let saved = 0;
+  let cash = {};
 
   dfs(storage, robots, 1);
 
@@ -87,21 +89,27 @@ function simulateBlueprint(blueprint, maxMinutes) {
 
   // ore, clay, obsidian, geode
   function dfs(storage, robots, minute) {
+    let key = `${storage.join(',')},${robots.join(',')},${minute}`;
+    if (key in cash) {
+      return cash[key];
+    }
     if (minute > maxMinutes) {
       if (storage[3] > maxGeo) {
         maxGeo = storage[3];
       }
-      return;
+      return storage[3];
     }
 
     const isCanBuild = canBuild(storage, robots, blueprint, maxMinutes - minute);
+    let locMax = 0;
 
     if (isCanBuild.geodeRobot) {
       let newRobots = [...robots];
       newRobots[3]++;
       let storageAfterBuild = buildRobot(storage, blueprint.geodeRobot);
       let storageAfterCollect = collectOres(storageAfterBuild, robots);
-      dfs(storageAfterCollect, newRobots, minute + 1);
+
+      locMax = Math.max(locMax, dfs(storageAfterCollect, newRobots, minute + 1));
     } else {
 
       if (isCanBuild.obsidianRobot) {
@@ -109,7 +117,7 @@ function simulateBlueprint(blueprint, maxMinutes) {
         newRobots[2]++;
         let storageAfterBuild = buildRobot(storage, blueprint.obsidianRobot);
         let storageAfterCollect = collectOres(storageAfterBuild, robots);
-        dfs(storageAfterCollect, newRobots, minute + 1);
+        locMax = Math.max(locMax, dfs(storageAfterCollect, newRobots, minute + 1));
       }
 
       if (isCanBuild.clayRobot) {
@@ -117,7 +125,7 @@ function simulateBlueprint(blueprint, maxMinutes) {
         newRobots[1]++;
         let storageAfterBuild = buildRobot(storage, blueprint.clayRobot);
         let storageAfterCollect = collectOres(storageAfterBuild, robots);
-        dfs(storageAfterCollect, newRobots, minute + 1);
+        locMax = Math.max(locMax, dfs(storageAfterCollect, newRobots, minute + 1));
       }
 
       if (isCanBuild.oreRobot) {
@@ -125,23 +133,27 @@ function simulateBlueprint(blueprint, maxMinutes) {
         newRobots[0]++;
         let storageAfterBuild = buildRobot(storage, blueprint.oreRobot);
         let storageAfterCollect = collectOres(storageAfterBuild, robots);
-        dfs(storageAfterCollect, newRobots, minute + 1);
+        locMax = Math.max(locMax, dfs(storageAfterCollect, newRobots, minute + 1));
       }
 
       let newRobots2 = [...robots];
       let storageAfterCollect2 = collectOres(storage, robots);
-      dfs(storageAfterCollect2, newRobots2, minute + 1);
+      locMax = Math.max(locMax, dfs(storageAfterCollect2, newRobots2, minute + 1));
     }
+
+    cash[key] = locMax;
+
+    return locMax;
   }
+
 }
 
 function collectOres(storage, robots) {
-  let newStorage = [...storage];
-  newStorage[0] += robots[0];
-  newStorage[1] += robots[1];
-  newStorage[2] += robots[2];
-  newStorage[3] += robots[3];
-  return newStorage;
+  storage[0] += robots[0];
+  storage[1] += robots[1];
+  storage[2] += robots[2];
+  storage[3] += robots[3];
+  return storage;
 }
 
 function buildRobot(storage, robotRequirements) {
@@ -189,39 +201,3 @@ robots:   ${robots.map((it) => String(it).padStart(4, ' ')).join('  ')}
 can build    ${+isCanBuild.oreRobot}     ${+isCanBuild.clayRobot}     ${+isCanBuild.obsidianRobot}     ${+isCanBuild.geodeRobot}
 `);
 }
-
-/*
-
-correct:
-2193
-blueprint 1 1
-blueprint 2 0
-blueprint 3 1
-blueprint 4 1
-blueprint 5 0
-blueprint 6 0
-blueprint 7 0
-blueprint 8 1
-blueprint 9 10
-blueprint 10 5
-blueprint 11 3
-blueprint 12 5
-blueprint 13 3
-blueprint 14 0
-blueprint 15 0
-blueprint 16 5
-blueprint 17 9
-blueprint 18 7
-blueprint 19 3
-blueprint 20 5
-blueprint 21 0
-blueprint 22 0
-blueprint 23 15
-blueprint 24 13
-blueprint 25 6
-blueprint 26 3
-blueprint 27 2
-blueprint 28 0
-blueprint 29 0
-blueprint 30 15
-*/
