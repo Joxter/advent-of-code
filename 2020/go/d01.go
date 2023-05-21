dc0f8161391e783b285447820a399e93025e7ed1
Joxter <joxterrr@gmail.com>
2023-05-21 11:00:33 +0200

    go fmt, TAAAABS T_T

--- 2020/go/d01.go
+++ 2020/go/d01.go
@@ -1,62 +1,57 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    inp, _ := os.ReadFile("../inputs/d01/input.txt")
+	inp, _ := os.ReadFile("../inputs/d01/input.txt")
 
-    res1 := part1(string(inp))
-    fmt.Println("part1", res1, res1 == 1020036)
+	res1 := part1(string(inp))
+	fmt.Println("part1", res1, res1 == 1020036)
 
-    res2 := part2(string(inp))
-    fmt.Println("part2", res2, res2 == 286977330)
+	res2 := part2(string(inp))
+	fmt.Println("part2", res2, res2 == 286977330)
 }
 
 func part1(inp string) int {
-    rawNums := strings.Split(inp, "\n")
+	rawNums := strings.Split(inp, "\n")
 
-    m := make(map[int]int)
-    for _, it := range rawNums {
-        num, _ := strconv.Atoi(it)
-        n, has := m[2020 - num]
-        if has {
-            return num * n
-        }
-        m[num] = num
-    }
+	m := make(map[int]int)
+	for _, it := range rawNums {
+		num, _ := strconv.Atoi(it)
+		n, has := m[2020-num]
+		if has {
+			return num * n
+		}
+		m[num] = num
+	}
 
-    return 0
+	return 0
 }
 
 func part2(inp string) int {
-    rawNums := strings.Split(inp, "\n")
+	rawNums := strings.Split(inp, "\n")
 
-    m := make(map[int]int)
-    var nums []int
-    for _, it := range rawNums {
-        num, _ := strconv.Atoi(it)
-        m[num] = num
-        nums = append(nums, num)
-    }
+	m := make(map[int]int)
+	var nums []int
+	for _, it := range rawNums {
+		num, _ := strconv.Atoi(it)
+		m[num] = num
+		nums = append(nums, num)
+	}
 
-    for i := 0; i < len(m) - 2; i++ {
-        for j := i + 1; j < len(m) - 1; j++ {
-            n, has := m[2020 - nums[i] - nums[j]]
-            if has {
-                return n * nums[i] * nums[j]
-            }
-        }
-    }
+	for i := 0; i < len(m)-2; i++ {
+		for j := i + 1; j < len(m)-1; j++ {
+			n, has := m[2020-nums[i]-nums[j]]
+			if has {
+				return n * nums[i] * nums[j]
+			}
+		}
+	}
 
-    return 0
+	return 0
 }
-
-
-
-
-
--- 2020/go/d02.go
+++ 2020/go/d02.go
@@ -1,79 +1,75 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    inp, _ := os.ReadFile("../inputs/d02/input.txt")
+	inp, _ := os.ReadFile("../inputs/d02/input.txt")
 
-    res1 := part1(string(inp))
-    fmt.Println("part1", res1, res1 == 445)
+	res1 := part1(string(inp))
+	fmt.Println("part1", res1, res1 == 445)
 
-    res2 := part2(string(inp))
-    fmt.Println("part2", res2, res2 == 491)
+	res2 := part2(string(inp))
+	fmt.Println("part2", res2, res2 == 491)
 }
 
 func part1(inp string) int {
-    lines := strings.Split(inp, "\n")
-    res := 0
+	lines := strings.Split(inp, "\n")
+	res := 0
 
-    for _, line := range lines {
-        p := strings.Split(line, ": ")
-        password := p[1]
+	for _, line := range lines {
+		p := strings.Split(line, ": ")
+		password := p[1]
 
-        ch := string(p[0][len(p[0]) - 1])
-        times := strings.Split(p[0][0:len(p[0]) - 2], "-")
+		ch := string(p[0][len(p[0])-1])
+		times := strings.Split(p[0][0:len(p[0])-2], "-")
 
-        cnt := count(password, ch)
-        min, _ := strconv.Atoi(times[0])
-        max, _ := strconv.Atoi(times[1])
+		cnt := count(password, ch)
+		min, _ := strconv.Atoi(times[0])
+		max, _ := strconv.Atoi(times[1])
 
-        if (cnt >= min  && cnt <= max ) {
-            res++
-        }
-    }
+		if cnt >= min && cnt <= max {
+			res++
+		}
+	}
 
-    return res
+	return res
 }
 
 func part2(inp string) int {
-    lines := strings.Split(inp, "\n")
-    res := 0
+	lines := strings.Split(inp, "\n")
+	res := 0
 
-    for _, line := range lines {
-        p := strings.Split(line, ": ")
-        password := p[1]
+	for _, line := range lines {
+		p := strings.Split(line, ": ")
+		password := p[1]
 
-        ch := string(p[0][len(p[0]) - 1])
-        times := strings.Split(p[0][0:len(p[0]) - 2], "-")
+		ch := string(p[0][len(p[0])-1])
+		times := strings.Split(p[0][0:len(p[0])-2], "-")
 
-        first, _ := strconv.Atoi(times[0])
-        second, _ := strconv.Atoi(times[1])
+		first, _ := strconv.Atoi(times[0])
+		second, _ := strconv.Atoi(times[1])
 
-        if (string(password[first - 1]) == ch) != (string(password[second - 1]) == ch) {
-            res++
-        }
-    }
+		if (string(password[first-1]) == ch) != (string(password[second-1]) == ch) {
+			res++
+		}
+	}
 
-    return res
+	return res
 }
 
 func count(s string, str string) int {
-    cnt := 0
+	cnt := 0
 
-    for _, v := range s {
-        if string(v) == str {
-            cnt++
-        }
-    }
+	for _, v := range s {
+		if string(v) == str {
+			cnt++
+		}
+	}
 
-    return cnt
+	return cnt
 }
-
-
-
-
