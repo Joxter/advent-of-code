97a17e23558f3fbb28df7599c9542264e29c5977
Joxter <joxterrr@gmail.com>
2023-06-03 22:03:53 +0200

    just copied naive

--- rust/src/aoc_day.rs
+++ rust/src/aoc_day.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::fmt::{Debug, Display, Formatter};
 use std::hint::black_box;
 use std::io::Write;
--- rust/src/y2022/day16.rs
+++ rust/src/y2022/day16.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 /*
-todo fix the worst code ever
+the worst code ever
 */
 
 pub fn naive_js_copy_part1(input: &str) -> i32 {
@@ -220,5 +220,290 @@ fn find_path<'a>(map: &HashMap<&str, Valve>, start: &str, finish: &'a str) -> (&
                 dfs(results, new_path, score + 1, finish, map);
             }
         }
+    }
+}
+
+pub mod optimised {
+    use std::cmp::Ordering;
+    use std::collections::{BinaryHeap, HashMap, VecDeque};
+    use std::usize;
+
+    pub fn better_part1(input: &str) -> i32 {
+        let (new_map, all_opened, heads) = parse(input);
+
+        return dfs(&new_map, "AA", 0, 0, 1, all_opened, &heads);
+
+        fn dfs(
+            new_map: &HashMap<&str, Valve2>,
+            node_name: &str,
+            released: i32,
+            opened: i32,
+            minutes: i32,
+            all_opened: i32,
+            heads: &HashMap<&str, i32>,
+        ) -> i32 {
+            let max_mins = 30;
+            if opened == all_opened || minutes > max_mins {
+                return released;
+            }
+
+            new_map
+                .get(node_name)
+                .unwrap()
+                .next
+                .iter()
+                .filter(|nod_name| {
+                    return nod_name.0 == "AA" || (opened & heads.get(nod_name.0).unwrap()) == 0;
+                })
+                .map(|(nod_name, cost)| {
+                    let rate = new_map.get(nod_name).unwrap().rate;
+                    let left_mins = max_mins - (minutes + cost);
+
+                    let new_opened = if *nod_name == "AA" {
+                        opened
+                    } else {
+                        heads.get(nod_name).unwrap() | opened
+                    };
+
+                    dfs(
+                        new_map,
+                        nod_name,
+                        released + rate * left_mins,
+                        new_opened,
+                        minutes + cost + 1,
+                        all_opened,
+                        heads,
+                    )
+                })
+                .max()
+                .unwrap()
+        }
+    }
+
+    pub fn better_part2(input: &str) -> i32 {
+        let (new_map, all_opened, heads) = parse(input);
+        let mut res = 0;
+        let max = all_opened / 2;
+
+        for i in 0..max {
+            let opened1 = i;
+
+            // if opened1.count_ones() < 7 {
+            //     continue;
+            // }
+            // if (opened1 ^ all_opened).count_ones() < 7 {
+            //     continue;
+            // }
+
+            let res1 = dfs(&new_map, "AA", 0, opened1, 1, all_opened, &heads);
+            let res2 = dfs(
+                &new_map,
+                "AA",
+                0,
+                opened1 ^ all_opened,
+                1,
+                all_opened,
+                &heads,
+            );
+
+            res = i32::max(res, res1 + res2);
+        }
+
+        return res;
+
+        fn dfs(
+            new_map: &HashMap<&str, Valve2>,
+            node_name: &str,
+            released: i32,
+            opened: i32,
+            minutes: i32,
+            all_opened: i32,
+            heads: &HashMap<&str, i32>,
+        ) -> i32 {
+            let max_mins = 26;
+            if opened == all_opened || minutes > max_mins {
+                return released;
+            }
+
+            new_map
+                .get(node_name)
+                .unwrap()
+                .next
+                .iter()
+                .filter(|nod_name| {
+                    return nod_name.0 == "AA" || (opened & heads.get(nod_name.0).unwrap()) == 0;
+                })
+                .map(|(nod_name, cost)| {
+                    let rate = new_map.get(nod_name).unwrap().rate;
+                    let left_mins = max_mins - (minutes + cost);
+
+                    let new_opened = if *nod_name == "AA" {
+                        opened
+                    } else {
+                        heads.get(nod_name).unwrap() | opened
+                    };
+
+                    dfs(
+                        new_map,
+                        nod_name,
+                        released + rate * left_mins,
+                        new_opened,
+                        minutes + cost + 1,
+                        all_opened,
+                        heads,
+                    )
+                })
+                .max()
+                .unwrap()
+        }
+    }
+
+    #[derive(Debug)]
+    struct Valve<'a> {
+        rate: i32,
+        next: Vec<&'a str>,
+        name: &'a str,
+    }
+
+    #[derive(Debug)]
+    struct Valve2<'a> {
+        rate: i32,
+        next: Vec<(&'a str, i32)>,
+        name: &'a str,
+    }
+
+    fn parse(input: &str) -> (HashMap<&str, Valve2>, i32, HashMap<&str, i32>) {
+        let mut map: HashMap<&str, Valve> = HashMap::new();
+
+        let mut max_valves = 0;
+        let mut heads: HashMap<&str, i32> = HashMap::new();
+
+        input.lines().for_each(|line| {
+            let parts = Vec::from_iter(line.split_whitespace());
+
+            let name = parts[1];
+            let rate = parts[4][5..parts[4].len() - 1].parse::<i32>().unwrap();
+            let next = Vec::from_iter(parts[9..].iter().map(|n| &n[0..2]));
+
+            if rate > 0 && name != "AA" {
+                heads.insert(name, 1 << max_valves);
+                max_valves += 1;
+            }
+
+            map.insert(name, Valve { rate, next, name });
+        });
+        let all_opened = i32::pow(2, max_valves) - 1;
+
+        let new_map = transform_map(&map);
+
+        (new_map, all_opened, heads)
+    }
+
+    fn transform_map<'a, 'b>(
+        map: &HashMap<&'a str, Valve<'b>>,
+    ) -> HashMap<&'b str, Valve2<'b>> {
+        let mut val_heads_names = vec!["AA"];
+
+        val_heads_names.extend(map.iter().filter(|(_, v)| v.rate > 0).map(|(_, v)| v.name));
+
+        let mut new_map: HashMap<&str, Valve2> = HashMap::new();
+
+        for from_name in &val_heads_names {
+            let mut next = vec![];
+            for to_name in &val_heads_names {
+                if from_name != to_name {
+                    next.push((*to_name, find_path(map, from_name, to_name)));
+                }
+            }
+
+            new_map.insert(
+                from_name,
+                Valve2 {
+                    name: from_name,
+                    rate: map.get(from_name).unwrap().rate,
+                    next,
+                },
+            );
+        }
+
+        new_map
+    }
+
+    fn find_path(map: &HashMap<&str, Valve>, start: &str, finish: &str) -> i32 {
+        let mut min = i32::MAX;
+
+        dfs(&mut min, vec![start], 0, finish, map);
+
+        return min;
+
+        fn dfs(
+            results: &mut i32,
+            path: Vec<&str>,
+            score: i32,
+            finish: &str,
+            map: &HashMap<&str, Valve>,
+        ) {
+            let last = path.last().unwrap();
+            if *last == finish {
+                *results = i32::min(*results, score);
+                return;
+            }
+            if score > *results {
+                return;
+            }
+
+            for node_name in &map.get(last).unwrap().next {
+                if !path.contains(node_name) {
+                    let mut new_path = path.clone();
+                    new_path.push(node_name);
+                    dfs(results, new_path, score + 1, finish, map);
+                }
+            }
+        }
+
+        /*        let mut heap = BinaryHeap::new(); // 2.840701s
+                let mut max_len = 0;
+
+                #[derive(Copy, Clone, Eq, PartialEq)]
+                struct State<'a> {
+                    cost: i32,
+                    name: &'a str,
+                }
+
+                impl<'a> Ord for State<'a> {
+                    fn cmp(&self, other: &Self) -> Ordering {
+                        other.cost.cmp(&self.cost)
+                    }
+                }
+                impl<'a> PartialOrd for State<'a> {
+                    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                        Some(self.cmp(other))
+                    }
+                }
+
+                heap.push(State {
+                    cost: 0,
+                    name: start,
+                });
+
+                while let Some(State { name, cost }) = heap.pop() {
+                    max_len = usize::max(max_len, heap.len());
+
+                    if name == finish {
+                        // println!("max_len: {}", max_len);
+                        return cost;
+                    }
+
+                    for next_node_name in &map.get(name).unwrap().next {
+                        // heap.push((next_node_name, score + 1));
+                        heap.push(State {
+                            cost: cost + 1,
+                            name: next_node_name,
+                        });
+                    }
+                }
+
+                unreachable!();
+        */
     }
 }
