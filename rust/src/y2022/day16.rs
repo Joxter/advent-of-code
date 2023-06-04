aa57edb88f6a90a0b227948c6a04215519b26e91
Joxter <joxterrr@gmail.com>
2023-06-04 22:22:54 +0200

    remove cheat:(

--- rust/src/y2022/day16.rs
+++ rust/src/y2022/day16.rs
@@ -230,12 +230,12 @@ pub mod optimised {
 
     // ideas
     //   - sort "new_map.get(node_name).unwrap().next" to go closest Valves first
+    //   - go by closest points + realise I can't get more than I already have !!!
     //   - save sum of all rates, like it is a maximum impossible score
 
     pub fn better_part1(input: &str) -> i32 {
-        let (new_map, all_opened, heads, total_rate) = parse(input);
+        let (new_map, _all_opened, heads) = parse(input);
 
-        // println!("total_rate: {}", total_rate); // 261
         // println!("heads: {:?}", heads);
         // println!("new_map: {:?}", new_map);
 
@@ -244,37 +244,40 @@ pub mod optimised {
         let max_mins = 30;
 
         let mut max_released = 0; // 2250
-                                  // let mut run_out_of_time = 0; // 4_703_853
 
         /*
-        new_map: {"UU": Valve2 { rate: 24, next: [("AA", 6), ("CO", 8), ("RU", 11), ("ZM", 7), ("WH", 7), ("UD", 3), ("PL", 5), ("FD", 5), ("DW", 8), ("OZ", 5), ("YJ", 3), ("EW", 6), ("MJ", 8), ("WJ", 8), ("ZI", 2)], name: "UU" }, "UD": Valve2 { rate: 19, next: [("AA", 7), ("CO", 9), ("RU", 8), ("ZM", 10), ("WH", 8), ("PL", 2), ("FD", 8), ("DW", 5), ("OZ", 8), ("YJ", 6), ("EW", 3), ("MJ", 7), ("WJ", 5), ("UU", 3), ("ZI", 5)], name: "UD" }, "WJ": Valve2 { rate: 25, next: [("AA", 10), ("CO", 12), ("RU", 11), ("ZM", 15), ("WH", 11), ("UD", 5), ("PL", 3), ("FD", 13), ("DW", 8), ("OZ", 13), ("YJ", 11), ("EW", 6), ("MJ", 10), ("UU", 8), ("ZI", 10)], name: "WJ" }, "FD": Valve2 { rate: 21, next: [("AA", 6), ("CO", 6), ("RU", 10), ("ZM", 2), ("WH", 4), ("UD", 8), ("PL", 10), ("DW", 7), ("OZ", 2), ("YJ", 5), ("EW", 9), ("MJ", 5), ("WJ", 13), ("UU", 5), ("ZI", 3)], name: "FD" }, "MJ": Valve2 { rate: 6, next: [("AA", 2), ("CO", 2), ("RU", 5), ("ZM", 7), ("WH", 3), ("UD", 7), ("PL", 7), ("FD", 5), ("DW", 2), ("OZ", 3), ("YJ", 5), ("EW", 4), ("WJ", 10), ("UU", 8), ("ZI", 6)], name: "MJ" }, "ZM": Valve2 { rate: 23, next: [("AA", 8), ("CO", 8), ("RU", 12), ("WH", 6), ("UD", 10), ("PL", 12), ("FD", 2), ("DW", 9), ("OZ", 4), ("YJ", 7), ("EW", 11), ("MJ", 7), ("WJ", 15), ("UU", 7), ("ZI", 5)], name: "ZM" }, "PL": Valve2 { rate: 22, next: [("AA", 7), ("CO", 9), ("RU", 8), ("ZM", 12), ("WH", 8), ("UD", 2), ("FD", 10), ("DW", 5), ("OZ", 10), ("YJ", 8), ("EW", 3), ("MJ", 7), ("WJ", 3), ("UU", 5), ("ZI", 7)], name: "PL" }, "YJ": Valve2 { rate: 15, next: [("AA", 3), ("CO", 5), ("RU", 8), ("ZM", 7), ("WH", 5), ("UD", 6), ("PL", 8), ("FD", 5), ("DW", 5), ("OZ", 3), ("EW", 7), ("MJ", 5), ("WJ", 11), ("UU", 3), ("ZI", 5)], name: "YJ" }, "AA": Valve2 { rate: 0, next: [("CO", 2), ("RU", 5), ("ZM", 8), ("WH", 2), ("UD", 7), ("PL", 7), ("FD", 6), ("DW", 2), ("OZ", 4), ("YJ", 3), ("EW", 4), ("MJ", 2), ("WJ", 10), ("UU", 6), ("ZI", 7)], name: "AA" }, "ZI": Valve2 { rate: 20, next: [("AA", 7), ("CO", 7), ("RU", 11), ("ZM", 5), ("WH", 5), ("UD", 5), ("PL", 7), ("FD", 3), ("DW", 8), ("OZ", 3), ("YJ", 5), ("EW", 8), ("MJ", 6), ("WJ", 10), ("UU", 2)], name: "ZI" }, "EW": Valve2 { rate: 16, next: [("AA", 4), ("CO", 6), ("RU", 5), ("ZM", 11), ("WH", 5), ("UD", 3), ("PL", 3), ("FD", 9), ("DW", 2), ("OZ", 7), ("YJ", 7), ("MJ", 4), ("WJ", 6), ("UU", 6), ("ZI", 8)], name: "EW" }, "DW": Valve2 { rate: 10, next: [("AA", 2), ("CO", 4), ("RU", 3), ("ZM", 9), ("WH", 3), ("UD", 5), ("PL", 5), ("FD", 7), ("OZ", 5), ("YJ", 5), ("EW", 2), ("MJ", 2), ("WJ", 8), ("UU", 8), ("ZI", 8)], name: "DW" }, "RU": Valve2 { rate: 14, next: [("AA", 5), ("CO", 7), ("ZM", 12), ("WH", 6), ("UD", 8), ("PL", 8), ("FD", 10), ("DW", 3), ("OZ", 8), ("YJ", 8), ("EW", 5), ("MJ", 5), ("WJ", 11), ("UU", 11), ("ZI", 11)], name: "RU" }, "WH": Valve2 { rate: 11, next: [("AA", 2), ("CO", 2), ("RU", 6), ("ZM", 6), ("UD", 8), ("PL", 8), ("FD", 4), ("DW", 3), ("OZ", 2), ("YJ", 5), ("EW", 5), ("MJ", 3), ("WJ", 11), ("UU", 7), ("ZI", 5)], name: "WH" }, "OZ": Valve2 { rate: 17, next: [("AA", 4), ("CO", 4), ("RU", 8), ("ZM", 4), ("WH", 2), ("UD", 8), ("PL", 10), ("FD", 2), ("DW", 5), ("YJ", 3), ("EW", 7), ("MJ", 3), ("WJ", 13), ("UU", 5), ("ZI", 3)], name: "OZ" }, "CO": Valve2 { rate: 18, next: [("AA", 2), ("RU", 7), ("ZM", 8), ("WH", 2), ("UD", 9), ("PL", 9), ("FD", 6), ("DW", 4), ("OZ", 4), ("YJ", 5), ("EW", 6), ("MJ", 2), ("WJ", 12), ("UU", 8), ("ZI", 7)], name: "CO" }}
-        */
+                new_map: {
+                   "DW": { rate: 10, next: [("CO", 4), ("RU", 3), ("ZM", 9), ("WH", 3), ("UD", 5), ("PL", 5), ("FD", 7), ("OZ", 5), ("YJ", 5), ("EW", 2), ("MJ", 2), ("WJ", 8), ("UU", 8), ("ZI", 8)], name: "DW" },
+                   "RU": { rate: 14, next: [("CO", 7), ("ZM", 12), ("WH", 6), ("UD", 8), ("PL", 8), ("FD", 10), ("DW", 3), ("OZ", 8), ("YJ", 8), ("EW", 5), ("MJ", 5), ("WJ", 11), ("UU", 11), ("ZI", 11)], name: "RU" },
+                   "YJ": { rate: 15, next: [("CO", 5), ("RU", 8), ("ZM", 7), ("WH", 5), ("UD", 6), ("PL", 8), ("FD", 5), ("DW", 5), ("OZ", 3), ("EW", 7), ("MJ", 5), ("WJ", 11), ("UU", 3), ("ZI", 5)], name: "YJ" },
+                   "AA": { rate: 0, next: [("CO", 2), ("RU", 5), ("ZM", 8), ("WH", 2), ("UD", 7), ("PL", 7), ("FD", 6), ("DW", 2), ("OZ", 4), ("YJ", 3), ("EW", 4), ("MJ", 2), ("WJ", 10), ("UU", 6), ("ZI", 7)], name: "AA" },
+                   "MJ": { rate: 6, next: [("CO", 2), ("RU", 5), ("ZM", 7), ("WH", 3), ("UD", 7), ("PL", 7), ("FD", 5), ("DW", 2), ("OZ", 3), ("YJ", 5), ("EW", 4), ("WJ", 10), ("UU", 8), ("ZI", 6)], name: "MJ" },
+                   "ZM": { rate: 23, next: [("CO", 8), ("RU", 12), ("WH", 6), ("UD", 10), ("PL", 12), ("FD", 2), ("DW", 9), ("OZ", 4), ("YJ", 7), ("EW", 11), ("MJ", 7), ("WJ", 15), ("UU", 7), ("ZI", 5)], name: "ZM" },
+                   "WJ": { rate: 25, next: [("CO", 12), ("RU", 11), ("ZM", 15), ("WH", 11), ("UD", 5), ("PL", 3), ("FD", 13), ("DW", 8), ("OZ", 13), ("YJ", 11), ("EW", 6), ("MJ", 10), ("UU", 8), ("ZI", 10)], name: "WJ" },
+                  +"UU": { rate: 24, next: [("CO", 8), ("RU", 11), ("ZM", 7), ("WH", 7), ("UD", 3), ("PL", 5), ("FD", 5), ("DW", 8), ("OZ", 5), ("YJ", 3), ("EW", 6), ("MJ", 8), ("WJ", 8), ("ZI", 2)], name: "UU" },
+                  +"UD": { rate: 19, next: [("CO", 9), ("RU", 8), ("ZM", 10), ("WH", 8), ("PL", 2), ("FD", 8), ("DW", 5), ("OZ", 8), ("YJ", 6), ("EW", 3), ("MJ", 7), ("WJ", 5), ("UU", 3), ("ZI", 5)], name: "UD" },
+                  +"FD": { rate: 21, next: [("CO", 6), ("RU", 10), ("ZM", 2), ("WH", 4), ("UD", 8), ("PL", 10), ("DW", 7), ("OZ", 2), ("YJ", 5), ("EW", 9), ("MJ", 5), ("WJ", 13), ("UU", 5), ("ZI", 3)], name: "FD" },
+                  +"PL": { rate: 22, next: [("CO", 9), ("RU", 8), ("ZM", 12), ("WH", 8), ("UD", 2), ("FD", 10), ("DW", 5), ("OZ", 10), ("YJ", 8), ("EW", 3), ("MJ", 7), ("WJ", 3), ("UU", 5), ("ZI", 7)], name: "PL" },
+                  +"ZI": { rate: 20, next: [("CO", 7), ("RU", 11), ("ZM", 5), ("WH", 5), ("UD", 5), ("PL", 7), ("FD", 3), ("DW", 8), ("OZ", 3), ("YJ", 5), ("EW", 8), ("MJ", 6), ("WJ", 10), ("UU", 2)], name: "ZI" },
+                  +"EW": { rate: 16, next: [("CO", 6), ("RU", 5), ("ZM", 11), ("WH", 5), ("UD", 3), ("PL", 3), ("FD", 9), ("DW", 2), ("OZ", 7), ("YJ", 7), ("MJ", 4), ("WJ", 6), ("UU", 6), ("ZI", 8)], name: "EW" },
+                  +"WH": { rate: 11, next: [("CO", 2), ("RU", 6), ("ZM", 6), ("UD", 8), ("PL", 8), ("FD", 4), ("DW", 3), ("OZ", 2), ("YJ", 5), ("EW", 5), ("MJ", 3), ("WJ", 11), ("UU", 7), ("ZI", 5)], name: "WH" },
+                  +"OZ": { rate: 17, next: [("CO", 4), ("RU", 8), ("ZM", 4), ("WH", 2), ("UD", 8), ("PL", 10), ("FD", 2), ("DW", 5), ("YJ", 3), ("EW", 7), ("MJ", 3), ("WJ", 13), ("UU", 5), ("ZI", 3)], name: "OZ" },
+                  +"CO": { rate: 18, next: [("RU", 7), ("ZM", 8), ("WH", 2), ("UD", 9), ("PL", 9), ("FD", 6), ("DW", 4), ("OZ", 4), ("YJ", 5), ("EW", 6), ("MJ", 2), ("WJ", 12), ("UU", 8), ("ZI", 7)], name: "CO" }}
+                */
 
         while let Some((node_name, released, opened, minutes)) = stack.pop() {
-            // println!("{} {} {} {}", node_name, released, opened, minutes);
-            if minutes > max_mins && opened != all_opened {
-                // run_out_of_time += 1;
-                // continue;
-            }
-
-            // let opened_AA = path.iter().filter(|n| **n == "AA").count();
-            // if opened_AA > 1 {
-            //     // println!("opened_AA: {}", opened_AA);
-            //     continue;
-            // }
-
             if minutes > max_mins {
                 if released > max_released {
                     max_released = released;
                     // println!("{} {:b} {}", opened.count_ones(), opened, minutes);
                     // println!("-- {}", max_released);
                 }
-                // println!("open - {}", released);
                 continue;
             }
 
             for (nod_name, cost) in &new_map.get(node_name).unwrap().next {
-                if (opened & heads.get(nod_name).unwrap()) == 0 && *cost < 4 { // WHY???? 4
+                // todo cheat "*cost < 4" give a huge advantage  (time 150msec -> 1msec)
+                if (opened & heads.get(nod_name).unwrap()) == 0 {
                     let rate = new_map.get(nod_name).unwrap().rate;
                     let left_mins = max_mins - (minutes + cost);
 
@@ -294,7 +297,7 @@ pub mod optimised {
     }
 
     pub fn better_part2(input: &str) -> i32 {
-        let (new_map, all_opened, heads, _total_rate) = parse(input);
+        let (new_map, all_opened, heads) = parse(input);
         let mut res = 0;
         let max = all_opened / 2;
 
@@ -336,8 +339,9 @@ pub mod optimised {
                 .unwrap()
                 .next
                 .iter()
-                .filter(|(nod_name, cost)| {
-                    return *cost < 7 && (opened & heads.get(nod_name).unwrap()) == 0; // todo WHY 7 ???
+                .filter(|(nod_name, _cost)| {
+                    // todo cheat "*cost < 7" give a huge advantage  (time 33sec -> 7sec)
+                    return (opened & heads.get(nod_name).unwrap()) == 0;
                 })
                 .map(|(nod_name, cost)| {
                     let rate = new_map.get(nod_name).unwrap().rate;
@@ -356,7 +360,7 @@ pub mod optimised {
                     )
                 })
                 .max()
-                .unwrap()
+                .unwrap_or(0)
         }
     }
 
@@ -371,15 +375,13 @@ pub mod optimised {
     struct Valve2<'a> {
         rate: i32,
         next: Vec<(&'a str, i32)>,
-        name: &'a str,
     }
 
-    fn parse(input: &str) -> (HashMap<&str, Valve2>, i32, HashMap<&str, i32>, i32) {
+    fn parse(input: &str) -> (HashMap<&str, Valve2>, i32, HashMap<&str, i32>) {
         let mut map: HashMap<&str, Valve> = HashMap::new();
 
         let mut max_valves = 0;
         let mut heads: HashMap<&str, i32> = HashMap::new();
-        let mut total_rate = 0;
 
         input.lines().for_each(|line| {
             let parts = Vec::from_iter(line.split_whitespace());
@@ -391,7 +393,6 @@ pub mod optimised {
             if rate > 0 && name != "AA" {
                 heads.insert(name, 1 << max_valves);
                 max_valves += 1;
-                total_rate += rate;
             }
 
             map.insert(name, Valve { rate, next, name });
@@ -400,7 +401,7 @@ pub mod optimised {
 
         let new_map = transform_map(&map);
 
-        (new_map, all_opened, heads, total_rate)
+        (new_map, all_opened, heads)
     }
 
     fn transform_map<'a, 'b>(map: &HashMap<&'a str, Valve<'b>>) -> HashMap<&'b str, Valve2<'b>> {
@@ -422,7 +423,6 @@ pub mod optimised {
             new_map.insert(
                 from_name,
                 Valve2 {
-                    name: from_name,
                     rate: map.get(from_name).unwrap().rate,
                     next,
                 },
