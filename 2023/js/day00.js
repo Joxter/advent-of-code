45a16da31653c3ae1d43476a4b98d4a166c703da
Nikolai Morozov <nmorozov@apcreation.nl>
2023-11-20 12:27:20 +0100

    2023 preparations

---
+++ 2023/js/day00.js
@@ -0,0 +1,18 @@
+import { runDay } from '../../utils.js';
+
+// https://adventofcode.com/2023/day/***
+
+console.log(part1(`test`))
+
+runDay(2023, 1)
+  .part(1, part1)
+  .part(2, part2);
+
+function part1(inp) {
+  return 123;
+}
+
+function part2(inp) {
+  return 123;
+}
+
