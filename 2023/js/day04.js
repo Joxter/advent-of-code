0aeddbf0afe766eefc207193698f5292997d9a27
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-04 15:10:39 +0100

    2023/4 add recursive variant

--- 2023/js/day04.js
+++ 2023/js/day04.js
@@ -4,7 +4,8 @@ import { runDay, sum } from '../../utils.js';
 
 runDay(2023, 4)
   .part(1, part1)
-  .part(2, part2);
+  .part(2, part2)
+  .part(2, part2Recursive, 'recursive');
 
 function part1(inp) {
   let score = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512];
@@ -46,4 +47,37 @@ function part2(inp) {
   }
 
   return sum(cards);
+}
+
+function part2Recursive(inp) {
+  let points = inp
+    .split('\n')
+    .map((line) => {
+      let parts = line.split(':')[1];
+      let [winning, numbers] = parts.split('|');
+      winning = winning.trim().split(/\s+/);
+      numbers = numbers.trim().split(/\s+/);
+
+      let matches = winning.filter((n) => numbers.includes(n));
+      return matches.length;
+    });
+
+  // let cache = {}; cache makes the solution as fast as the original one
+
+  function play(i) {
+    // if (cache[i]) return cache[i];
+    let t = 1;
+    for (let j = 0; j < points[i]; j++) {
+      t += play(i + j + 1);
+    }
+    // cache[i] = t;
+    return t;
+  }
+
+  let total = 0;
+  for (let i = 0; i < points.length; i++) {
+    total += play(i);
+  }
+
+  return total;
 }
