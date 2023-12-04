54055e38af16ce3beb5198b40f8622346308970b
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-04 08:56:32 +0100

    2023/4 part2

--- 2023/js/day04.js
+++ 2023/js/day04.js
@@ -2,13 +2,6 @@ import { runDay, sum } from '../../utils.js';
 
 // https://adventofcode.com/2023/day/4
 
-console.log(part1(`Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`));
-
 runDay(2023, 4)
   .part(1, part1)
   .part(2, part2);
@@ -30,6 +23,26 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let points = inp
+    .split('\n')
+    .map((line) => {
+      let parts = line.split(':')[1];
+      let [winning, numbers] = parts.split('|');
+      winning = winning.trim().split(/\s+/);
+      numbers = numbers.trim().split(/\s+/);
+
+      let matches = winning.filter((n) => numbers.includes(n));
+      return matches.length;
+    });
+
+  let wins = Array(points.length).fill(1);
+
+  for (let i = 0; i < points.length; i++) {
+    for (let j = 0; j < points[i]; j++) {
+      wins[i + j + 1] += wins[i]
+    }
+  }
+
+  return sum(wins);
 }
 
