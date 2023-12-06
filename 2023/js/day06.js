66f49ec89ac931ec09bc5193ca7023532720bc43
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-06 08:52:40 +0100

    2023/6 add math and clean up

--- 2023/js/day06.js
+++ 2023/js/day06.js
@@ -4,7 +4,9 @@ import { runDay } from '../../utils.js';
 
 runDay(2023, 6)
   .part(1, part1)
-  .part(2, part2);
+  .part(1, part1math, 'math')
+  .part(2, part2)
+  .part(2, part2math, 'math');
 
 function part1(inp) {
   let [times, dists] = inp.split('\n');
@@ -17,7 +19,6 @@ function part1(inp) {
     let dist = dists[i];
 
     let wins = 0;
-    let lastDist = 0;
     for (let j = 1; j < time; j++) {
       let curDist = j * (time - j);
       if (curDist > dist) {
@@ -30,23 +31,45 @@ function part1(inp) {
   return total;
 }
 
-function part2(inp) {
-
+function part1math(inp) {
   let [times, dists] = inp.split('\n');
-  let time = +times.split(/\s+/).slice(1).join('');
-  let dist = +dists.split(/\s+/).slice(1).join('');
+  times = times.split(/\s+/).slice(1).map(Number);
+  dists = dists.split(/\s+/).slice(1).map(Number);
 
   let total = 1;
+  for (let i = 0; i < times.length; i++) {
+    total *= solve(-times[i], dists[i]);
+  }
+
+  return total;
+}
+
+function part2(inp) {
+  let [times, dists] = inp.split('\n');
+  let time = +times.replace(/\D/g, '');
+  let dist = +dists.replace(/\D/g, '');
 
   let wins = 0;
-  let lastDist = 0;
   for (let j = 1; j < time; j++) {
     let curDist = j * (time - j);
     if (curDist > dist) {
       wins++;
     }
   }
-  total *= wins;
 
-  return total;
+  return wins;
+}
+
+function part2math(inp) {
+  let [times, dists] = inp.split('\n');
+  let time = +times.replace(/\D/g, '');
+  let dist = +dists.replace(/\D/g, '');
+
+  return solve(-time, dist);
+}
+
+function solve(b, c) {
+  let d = Math.sqrt((b / 2) ** 2 - c);
+
+  return Math.floor(-b / 2 + d) - Math.ceil(-b / 2 - d) + 1;
 }
