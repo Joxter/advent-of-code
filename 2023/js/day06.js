83a9bf3f23df7d791613845f81249359b8591672
Joxter <joxterrr@gmail.com>
2023-12-06 06:52:44 +0100

    2023/6

---
+++ 2023/js/day06.js
@@ -0,0 +1,52 @@
+import { runDay } from '../../utils.js';
+
+// https://adventofcode.com/2023/day/6
+
+runDay(2023, 6)
+  .part(1, part1)
+  .part(2, part2);
+
+function part1(inp) {
+  let [times, dists] = inp.split('\n');
+  times = times.split(/\s+/).slice(1).map(Number);
+  dists = dists.split(/\s+/).slice(1).map(Number);
+
+  let total = 1;
+  for (let i = 0; i < times.length; i++) {
+    let time = times[i];
+    let dist = dists[i];
+
+    let wins = 0;
+    let lastDist = 0;
+    for (let j = 1; j < time; j++) {
+      let curDist = j * (time - j);
+      if (curDist > dist) {
+        wins++;
+      }
+    }
+    total *= wins;
+  }
+
+  return total;
+}
+
+function part2(inp) {
+
+  let [times, dists] = inp.split('\n');
+  let time = +times.split(/\s+/).slice(1).join('');
+  let dist = +dists.split(/\s+/).slice(1).join('');
+
+  let total = 1;
+
+  let wins = 0;
+  let lastDist = 0;
+  for (let j = 1; j < time; j++) {
+    let curDist = j * (time - j);
+    if (curDist > dist) {
+      wins++;
+    }
+  }
+  total *= wins;
+
+  return total;
+}
