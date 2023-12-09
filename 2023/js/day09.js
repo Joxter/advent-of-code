f88aa2c2c04e55c02c791739a33118abce91c676
Joxter <joxterrr@gmail.com>
2023-12-09 07:55:35 +0100

    2023/9

---
+++ 2023/js/day09.js
@@ -0,0 +1,55 @@
+import { runDay, sum } from '../../utils.js';
+
+// https://adventofcode.com/2023/day/9
+
+runDay(2023, 9)
+  .part(1, part1)
+  .part(2, part2);
+
+function part1(inp) {
+  let rows = inp
+    .split('\n')
+    .map((line) => line.split(' ').map(Number));
+
+  let predictions = rows
+    .map((row) => {
+      let n = row.at(-1);
+
+      while (!row.every(n => n === 0)) {
+        let newRow = Array(row.length - 1);
+        for (let i = 0; i < row.length - 1; i++) {
+          newRow[i] = row[i + 1] - row[i];
+        }
+        n += newRow.at(-1);
+        row = newRow;
+      }
+
+      return n;
+    });
+
+  return sum(predictions);
+}
+
+function part2(inp) {
+  let rows = inp
+    .split('\n')
+    .map((line) => line.split(' ').map(Number));
+
+  let predictions = rows
+    .map((row) => {
+      let history = [row[0]];
+
+      while (!row.every(n => n === 0)) {
+        let newRow = Array(row.length - 1);
+        for (let i = 0; i < row.length - 1; i++) {
+          newRow[i] = row[i + 1] - row[i];
+        }
+        history.push(newRow[0]);
+        row = newRow;
+      }
+
+      return history.reduceRight((acc, it) => it - acc, 0);
+    });
+
+  return sum(predictions);
+}
