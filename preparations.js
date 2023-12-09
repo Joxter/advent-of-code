60fa9940151e48f9cb56322f985db4f68fce7158
Joxter <joxterrr@gmail.com>
2023-12-09 21:30:45 +0100

    inputs/answers 2022 js refactoring

--- preparations.js
+++ preparations.js
@@ -3,16 +3,17 @@ import path from 'path';
 
 generate();
 
-function generate () {
+function generate() {
+  let newFolder = new URL(`./2021/answers/`, import.meta.url).pathname;
+  fs.mkdirSync(newFolder, { recursive: true });
+
   for (let day = 1; day <= 25; day++) {
     let DD = String(day).padStart(2, '0');
 
-    let oldFolder = new URL(`./2023/inputs/d${DD}`, import.meta.url).pathname;
+    let oldFolder = new URL(`./2021/day-${DD}`, import.meta.url).pathname;
     let p1 = fs.readFileSync(path.join(oldFolder, 'part1.txt'));
     let p2 = fs.readFileSync(path.join(oldFolder, 'part2.txt'));
 
-    let newFolder = new URL(`./2023/answers/`, import.meta.url).pathname;
-    fs.mkdirSync(newFolder, { recursive: true });
     fs.writeFileSync(path.join(newFolder, `day${DD}-part1.txt`), p1);
     fs.writeFileSync(path.join(newFolder, `day${DD}-part2.txt`), p2);
   }
