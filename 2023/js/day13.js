c3d02cb7482eb562893b9acf5f9bb4460b05cf36
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-13 11:11:34 +0100

    2023/13 part1 wip

--- 2023/js/day13.js
+++ 2023/js/day13.js
@@ -2,24 +2,25 @@ import { rotateGrid90, runDay, sum } from '../../utils.js';
 
 // https://adventofcode.com/2023/day/13
 
-console.log(part1(`#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#.
-
-#...##..#
-#....#..#
-..##..###
-#####.##.
-#####.##.
-..##..###
-#....#..#`));
+// console.log(part1(`#.##..##.
+// ..#.##.#.
+// ##......#
+// ##......#
+// ..#.##.#.
+// ..##..##.
+// #.#.##.#.
+//
+// #...##..#
+// #....#..#
+// ..##..###
+// #####.##.
+// #####.##.
+// ..##..###
+// #....#..#`));
 
 runDay(2023, 13)
-  // .part(1, part1, '34772 correct') //
+  .part(1, part1, '34772 correct') //
+  // .part(1, notMy2input, 'notMy2input') //
   // .part(1, notMy) /////////////// 34772,35554
   // .part(2, spart2)
   .end();
@@ -32,10 +33,11 @@ function part1(inp) {
   let patterns = (inp).split('\n\n');
 
   let rowsAndCols = patterns.map((pattern, i) => {
-    // console.log('-- Pattern', i);
+    console.log('-- Pattern', i);
     let my = mySolve(pattern);
 
-    console.log(my, sss(pattern, 0));
+    console.log(my, notMy2input(pattern));
+    // console.log(pattern, notMy2input(pattern));
     return my;
   });
   console.log(rowsAndCols);
@@ -49,13 +51,13 @@ function mySolve(pattern) {
   // console.log('');
 
   let horizontal = symmetry(lines);
-  console.log('horizontal ', horizontal);
+  // console.log('horizontal ', horizontal);
 
   let rotated = rotateGrid90(pattern.split('\n').map(l => l.split('')));
   lines = rotated.map(row => row.join(''));
 
   let vertical = symmetry(lines);
-  console.log('vertical   ', vertical);
+  // console.log('vertical   ', vertical);
 
   return horizontal.n * 100 + vertical.n;
 
@@ -93,84 +95,41 @@ function part2(inp) {
   return 123;
 }
 
-function sss(pattern, part) {
-  let grid = pattern.split("\n").map((l) => l.split(""));
+function notMy2input(pattern) {
 
-  let ressss = [grid, transpose(grid)].map((grid) => {
-    let d, j, I, J;
+  return run(parseInput(pattern))
 
-    (d = 0), (I = 0), (J = 0);
-    for (let i = 0.5; i < grid.length; i++) {
-      (j = 0.5), (d = 0);
-      while (true) {
-        if (i - j < 0 || i + j >= grid.length) break;
-        d += diff(grid[i - j], grid[i + j]);
-        if (d > part) break;
-        j++;
-      }
-      if (j > 0.5 && (i - j === -1 || i + j === grid.length) && d === part) {
-        I = i + 0.5;
-        J = j;
+  function parseInput(input) {
+    return input.split(/\n/);
+  }
+
+  function checkHorizontal(pattern, row) {
+    for (let i = row - 1, j = row; i >= 0 && j < pattern.length; i--, j++) {
+      if (pattern[i] !== pattern[j]) return false;
+    }
+    return true;
+  }
+
+  function transpose(pattern) {
+    const result = Array(pattern[0].length).fill("");
+    for (const row of pattern) {
+      [...row].forEach((c, i) => result[i] += c);
+    }
+    return result;
+  }
+
+  function run(pattern) {
+    for (let i = 1; i < pattern.length; i++) {
+      if (checkHorizontal(pattern, i)) {
+        return 100 * i;
       }
     }
-    return I;
-  });
-  console.log(ressss);
 
-  return ressss[0] * 100 + ressss[1];
-
-  function diff(a, b) {
-    return a.filter((c, i) => c !== b[i]).length;
-  }
-
-  function transpose(matrix) {
-    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
-  }
-
-}
-
-function notMy(input) {
-  return [process(parse(input), 0), process(parse(input), 1)];
-
-  function parse(input) {
-
-    const data = input
-      .trim()
-      .split("\n\n")
-      .map((grid) => grid.split("\n").map((l) => l.split("")));
-    return data;
-  }
-
-  function process(data, part) {
-    let d, j, I, J;
-    return data
-      .map((grid) => [grid, transpose(grid)])
-      .map((grids) => {
-        return grids.map((grid) => {
-          (d = 0), (I = 0), (J = 0);
-          for (let i = 0.5; i < grid.length; i++) {
-            (j = 0.5), (d = 0);
-            while (true) {
-              if (i - j < 0 || i + j >= grid.length) break;
-              d += diff(grid[i - j], grid[i + j]);
-              if (d > part) break;
-              j++;
-            }
-            if (j > 0.5 && (i - j === -1 || i + j === grid.length) && d === part) {
-              (I = i + 0.5), (J = j);
-            }
-          }
-          return I;
-        });
-      })
-      .reduce((acc, cur) => acc + cur[0] * 100 + cur[1], 0);
-  }
-
-  function transpose(matrix) {
-    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
-  }
-
-  function diff(a, b) {
-    return a.filter((c, i) => c !== b[i]).length;
+    const transposed = transpose(pattern);
+    for (let i = 1; i < transposed.length; i++) {
+      if (checkHorizontal(transposed, i)) {
+        return i;
+      }
+    }
   }
 }
