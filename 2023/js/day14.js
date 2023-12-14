c47b22fa8e7c13ce747c71e55bb25e3dd755a91a
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-14 08:51:42 +0100

    2023/14 part2 wip

--- 2023/js/day14.js
+++ 2023/js/day14.js
@@ -1,21 +1,21 @@
-import { forEachInGrid, printGrid, runDay } from '../../utils.js';
+import { forEachInGrid, formatTime, printGrid, runDay } from '../../utils.js';
 
 // https://adventofcode.com/2023/day/14
 
-console.log(part2(`O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....`));
+// console.log(part2(`O....#....
+// O.OO#....#
+// .....##...
+// OO.#O....O
+// .O.....O#.
+// O.#..O.#.#
+// ..O..#O..O
+// .......O..
+// #....###..
+// #OO..#....`));
 
 runDay(2023, 14)
-  // .part(1, part1, '110677')// 110677
-  // .part(2, part2)
+  .part(1, part1, '110677')// 110677
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -45,13 +45,14 @@ function part1(inp) {
 
 function part2(inp) {
   let grid = inp.split('\n').map(l => l.split(''));
-  let round = 'O';
-  // let limit = 1_000_000_000;
-  let limit = 3;
+  let rock = 'O';
+  let limit = 1_000_000_000;
+  // let limit = 3;
+  let stat = Date.now()
 
   for (let i = 1; i <= limit; i++) {
     if (i % 10_000 === 0) {
-      console.log('Progress %', i / 1_000_000_000 * 100);
+      console.log('Progress %', i / 1_000_000_000 * 100,'time:', formatTime(Date.now() - stat));
     }
     // console.log(i);
     // console.log();
@@ -60,13 +61,17 @@ function part2(inp) {
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[0].length; j++) {
         let cell = grid[i][j];
-        if (cell === round) {
+        if (cell === rock) {
           let currI = i;
           while (grid[currI - 1]?.[j] === '.') {
             currI--;
           }
-          grid[i][j] = '.';
-          grid[currI][j] = round;
+          let rCount = 0;
+          while (grid[i + rCount]?.[j] === rock) {
+            grid[i + rCount][j] = '.';
+            grid[currI + rCount][j] = rock;
+            rCount++;
+          }
         }
       }
     }
@@ -75,13 +80,17 @@ function part2(inp) {
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[0].length; j++) {
         let cell = grid[i][j];
-        if (cell === round) {
+        if (cell === rock) {
           let currJ = j;
           while (grid[i][currJ - 1] === '.') {
             currJ--;
           }
-          grid[i][j] = '.';
-          grid[i][currJ] = round;
+          let rCount = 0;
+          while (grid[i][j + rCount] === rock) {
+            grid[i][j + rCount] = '.';
+            grid[i][currJ + rCount] = rock;
+            rCount++;
+          }
         }
       }
     }
@@ -91,13 +100,17 @@ function part2(inp) {
     for (let i = grid.length - 1; i >= 0; i--) {
       for (let j = 0; j < grid[0].length; j++) {
         let cell = grid[i][j];
-        if (cell === round) {
+        if (cell === rock) {
           let currI = i;
           while (grid[currI + 1]?.[j] === '.') {
             currI++;
           }
-          grid[i][j] = '.';
-          grid[currI][j] = round;
+          let rCount = 0;
+          while (grid[i - rCount]?.[j] === rock) {
+            grid[i - rCount][j] = '.';
+            grid[currI - rCount][j] = rock;
+            rCount++;
+          }
         }
       }
     }
@@ -107,17 +120,21 @@ function part2(inp) {
     for (let i = 0; i < grid.length; i++) {
       for (let j = grid[0].length - 1; j >= 0; j--) {
         let cell = grid[i][j];
-        if (cell === round) {
+        if (cell === rock) {
           let currJ = j;
           while (grid[i][currJ + 1] === '.') {
             currJ++;
           }
-          grid[i][j] = '.';
-          grid[i][currJ] = round;
+          let rCount = 0;
+          while (grid[i][j - rCount] === rock) {
+            grid[i][j - rCount] = '.';
+            grid[i][currJ - rCount] = rock;
+            rCount++;
+          }
         }
       }
     }
-    console.log(printGrid(grid));
+    // console.log(printGrid(grid));
   }
 
 
@@ -125,7 +142,7 @@ function part2(inp) {
 
   let total = 0;
   forEachInGrid(grid, (cell, i) => {
-    if (cell === round) {
+    if (cell === rock) {
       total += height - i;
     }
   });
