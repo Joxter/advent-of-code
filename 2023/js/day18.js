759b2f17eb5603ffa1917a3ba4ddf5f6c8216da5
Nikolai Morozov <nmorozov@apcreation.nl>
2023-12-18 16:44:45 +0100

    2023/18 part2

--- 2023/js/day18.js
+++ 2023/js/day18.js
@@ -2,79 +2,13 @@ import { runDay } from '../../utils.js';
 
 // https://adventofcode.com/2023/day/18
 
-console.log(part2(`R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)`), [952408144115]); // 62, 952408144115
-
-// console.log(part1opt(`R 4 foo
-// D 4 foo
-// L 2 foo
-// D 2 foo
-// R 3 foo
-// D 2 foo
-// L 6 foo
-// U 5 foo
-// R 1 foo
-// U 3 foo`), [5 * 3 + 6 * 2 + 4 + 7 * 3]); // 62, 952408144115
 
 runDay(2023, 18)
-  // .part(1, part1)
+  .part(1, part1)
   .part(1, part1opt, 'optimized')
-  .part(2, part2, '62762509300692 high, 62762509300691, 62762509300690 wrong')
+  .part(1, part1correct, 'part1correct')
+  .part(2, part2)
   .end();
-
-/*
-  0123456
-
-0 .XXXXX. 0
-1 .X...X. 1
-2 .X...X. 2
-3 XX...X. 3
-4 X..XXX. 4
-5 X..X... 5
-6 X..XXXX 6
-7 X.....X 7
-8 XXXXXXX 8
-
-  0123456    0123456   012345
-
-0 .XXXXX. 0  .OOOO?. 0 .XXXX. 0
-1 .XXXXX. 1  .OOOO?. 1 .XXXX. 1
-2 .XXXXX. 2  .OOOO?. 2 .XXXX. 2
-3 XXXXXX. 3  OOOOO?. 3 XXXXX. 3
-4 XXXXXX. 4  OOO???. 4 XXX... 4
-5 XXXX... 5  OOO?... 5 XXX... 5
-6 XXXXXXX 6  OOOOOO? 6 XXXXXX 6
-7 XXXXXXX 7  OOOOOO? 7 XXXXXX 7
-8 XXXXXXX 8  ??????X 8
-
--- R4 D4 L2 D2 R3 D2   L6   U5 R1 U3
-
--- L4 D3 L1 D5 R6 U2 L3 U2 L2 U4  (revert)
-
-*/
-
-console.log(part1opt(`L 4 foo
-D 3 foo 
-L 1 foo 
-D 5 foo 
-R 6 foo 
-U 2 foo 
-L 3 foo 
-U 2 foo 
-R 2 foo 
-U 4 foo`), [52]);
 
 function part1(inp) {
   let trench = {};
@@ -119,7 +53,7 @@ function part1(inp) {
 
   let stack = [[-1, 1]]; // hardcode :(
 
-  let limit = 100;
+  let limit = 1000000;
 
   while (stack.length > 0) {
     limit--;
@@ -145,14 +79,36 @@ function part1opt(inp) {
   return countSize(parse1(inp));
 }
 
-/*
-L 4 (#6c74e0) LR
-D 4 (#1afab1)
-L 4 (#0b54a0)
-D 18 (#3533c1)
-*/
+function part1correct(inp) {
+  // https://en.wikipedia.org/wiki/Pick's_theorem
+  // https://en.wikipedia.org/wiki/Shoelace_formula
+  const dirs = { 'R': [1, 0], 'D': [0, 1], 'L': [-1, 0], 'U': [0, -1] };
+
+  let pos = 0;
+  let ans = 1;
+
+  parse1(inp).forEach(([d, count]) => {
+    let [x, y] = dirs[d];
+    pos += x * count;
+    ans += y * count * pos + count / 2;
+  })
+
+  return Math.floor(ans);
+}
+
 function part2(inp) {
-  return countSize(parse2(inp));
+  const dirs = { 'R': [1, 0], 'D': [0, 1], 'L': [-1, 0], 'U': [0, -1] };
+
+  let pos = 0;
+  let ans = 1;
+
+  parse2(inp).forEach(([d, count]) => {
+    let [x, y] = dirs[d];
+    pos += x * count;
+    ans += y * count * pos + count / 2;
+  })
+
+  return Math.floor(ans);
 }
 
 function countSize(steps) {
