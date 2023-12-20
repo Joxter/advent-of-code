6be5916da452ba459d8d59e1ee6b7cca638fb2cb
Joxter <joxterrr@gmail.com>
2023-12-20 21:27:19 +0100

    2023/20 part2

--- 2023/js/day20.js
+++ 2023/js/day20.js
@@ -1,10 +1,10 @@
-import { formatTime, runDay } from '../../utils.js';
+import { formatTime, lcm, runDay } from '../../utils.js';
 
 // https://adventofcode.com/2023/day/20
 
 runDay(2023, 20)
   .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -36,18 +36,14 @@ function part1(inp) {
       });
     });
 
-  let clicks = 1000;
   let cnt = { lo: 0, hi: 0 };
 
-  for (let i = 1; i <= clicks; i++) {
+  for (let i = 1; i <= 1000; i++) {
     let signals = [['lo', 'broadcaster']];
-
     let nextSignals = [];
-    let lim = 100;
-
     cnt.lo++;
 
-    while (signals.length > 0 && lim--) {
+    while (signals.length > 0) {
       nextSignals = [];
 
       let touchedCon = [];
@@ -104,18 +100,108 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let modules = {};
+  let flipStatus = {};
+  let mem = {};
+
+  let rxSender = null;
+
+  inp
+    .split('\n')
+    .forEach((line) => {
+      let [name, targets] = line.split(' -> ');
+      modules[name] = targets.split(', ');
+
+      if (targets === 'rx') {
+        rxSender = name.slice(1);
+      }
+
+      if (name[0] === '%') {
+        flipStatus[name] = 'off';
+      }
+      if (name[0] === '&') {
+        mem[name] = {};
+      }
+    });
+
+  let periods = {};
+
+  Object
+    .entries(modules)
+    .forEach(([sender, receivers]) => {
+      receivers.forEach((receiver) => {
+
+        if (receiver === rxSender) {
+          periods[sender] = null;
+        }
+        if (mem[getName(receiver)]) {
+          mem[getName(receiver)][sender] = 'lo';
+        }
+      });
+    });
+
+  let i = 0;
+  steps: while (true) {
+    i++;
+    let signals = [['lo', 'broadcaster']];
+
+    let nextSignals = [];
+
+    while (signals.length > 0) {
+      nextSignals = [];
+
+      let touchedCon = [];
+
+      for (let [power, senderName] of signals) {
+        let receivers = modules[senderName].map(t => getName(t));
+
+        for (let receiverName of receivers) {
+          if (power === 'lo' && receiverName in periods && !periods[receiverName]) {
+            periods[receiverName] = i;
+
+            if (Object.values(periods).every(it => it !== null)) break steps;
+          }
+
+          if (receiverName[0] === '%') {
+            if (power === "lo") {
+              if (flipStatus[receiverName] === 'on') {
+                flipStatus[receiverName] = "off";
+                nextSignals.push(["lo", receiverName]);
+              } else if (flipStatus[receiverName] === 'off') {
+                flipStatus[receiverName] = 'on';
+                nextSignals.push(["hi", receiverName]);
+              } else {
+                throw 'unreachable';
+              }
+            }
+          }
+
+          if (receiverName[0] === "&") {
+            mem[receiverName][senderName] = power;
+            touchedCon.push(receiverName);
+          }
+        }
+      }
+
+      touchedCon.forEach((r) => {
+        let history = mem[r];
+        if (Object.values(history).every((p) => p === "hi")) {
+          nextSignals.push(["lo", r]);
+        } else {
+          nextSignals.push(["hi", r]);
+        }
+      });
+
+      signals = nextSignals;
+    }
+  }
+
+  return lcm(Object.values(periods));
+
+  function getName(short) {
+    if (short === 'broadcaster') return 'broadcaster';
+    if (modules['%' + short]) return '%' + short;
+    if (modules['&' + short]) return '&' + short;
+    return short;
+  }
 }
-
-/*
-
-&kz remembers high pulses for all inputs, it sends a low pulse
-
-       &kz -> rx
-
-&bg -> kz
-&sj -> kz
-&qq -> kz
-&ls -> kz
-
-*/
