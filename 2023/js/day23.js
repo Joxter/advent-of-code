b62f88aac63bf2b8f851c40ee66670c3c34735b5
Joxter <joxterrr@gmail.com>
2023-12-23 08:14:51 +0100

    2023/23 part2 wip

--- 2023/js/day23.js
+++ 2023/js/day23.js
@@ -2,7 +2,7 @@ import { BLACK, makeGrid, printGrid, printGridCb, runDay } from '../../utils.js'
 
 // https://adventofcode.com/2023/day/23
 
-console.log(part1(`#.#####################
+console.log(part2(`#.#####################
 #.......#########...###
 #######.#########.#.###
 ###.....#.>.>.###.#.###
@@ -27,8 +27,8 @@ console.log(part1(`#.#####################
 #####################.#`));
 
 runDay(2023, 23)
-  .part(1, part1)
-  // .part(2, part2)
+  // .part(1, part1)
+  .part(2, part2, '1625 low')
   .end();
 
 function part1(inp) {
@@ -86,5 +86,65 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let grid = makeGrid(inp);
+
+  let w = grid[0].length;
+
+  grid[0][1] = '#';
+  grid.at(-1)[w - 2] = '#';
+
+  console.log(printGrid(grid));
+
+  let q = [[1, 1, [`1,1`]]];
+
+  let maxPath = [];
+
+  let limit = 100_000_000;
+
+  grid[0][1] = '#';
+  grid[grid.length - 2][grid[0].length - 2] = 'X';
+
+  while (q.length > 0 && limit--) {
+    // break
+    let [i, j, path] = q.shift();
+    // console.log([i,j, path]);
+    let value = grid[i][j];
+    let isFinal = i === grid.length - 2 && j === grid[0].length - 2;
+
+    if (value === '#') continue;
+    if (path.lastIndexOf(path.at(-1)) !== path.indexOf(path.at(-1))) {
+      continue;
+    }
+
+    if (isFinal) {
+      console.log('final', [maxPath.length], {limit}, q.length);
+      if (path.length > maxPath.length) {
+        maxPath = path
+      }
+    }
+
+    if (typeof value === "number" && value < path && value + 2 !== path) {
+      value = '.';
+    }
+
+    q.push([i, j - 1, [...path, `${i},${j - 1}`]]);
+    q.push([i, j + 1, [...path, `${i},${j + 1}`]]);
+    q.push([i - 1, j, [...path, `${i - 1},${j}`]]);
+    q.push([i + 1, j, [...path, `${i + 1},${j}`]]);
+
+    // max = Math.max(path.length, max);
+  }
+
+  console.log('');
+  console.log(printGridCb(grid, (cell, i,j) => {
+    if (maxPath.includes(`${i},${j}`)) return '0'
+
+    if (typeof cell === "number") return cell % 10;
+    if (cell === '#') return BLACK;
+    return cell;
+  }));
+
+  console.log({limit});
+  return maxPath.length + 1;
 }
+
