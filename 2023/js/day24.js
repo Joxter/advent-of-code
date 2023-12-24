4792a5a4c70912943a21d59bdb2c0a598f4a64b2
Joxter <joxterrr@gmail.com>
2023-12-24 12:59:28 +0100

    2023/24 part2 wip

--- 2023/js/day24.js
+++ 2023/js/day24.js
@@ -10,7 +10,7 @@ console.log(part2(`19, 13, 30 @ -2,  1, -2
 
 runDay(2023, 24)
   // .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp, area = [200000000000000, 400000000000000]) {
@@ -79,114 +79,26 @@ function part2(inp) {
     });
 
   for (let i = 0; i < hails.length; i++) {
-    let hail1 = hails[i];
+    // let hail1 = hails[i];
+    console.log(i, hails.length);
     for (let j = 0; j < hails.length; j++) {
       if (i === j) continue;
 
-      let hail2 = hails[j];
+      // let hail2 = hails[j];
 
-      let point1 = pointInTime(hail1, 1);
-      let point2 = pointInTime(hail2, 3);
-
-      let possibleLine1 = makeLineByDots(point2, point1);
-      let possibleLine2 = makeLineByDots(point1, point2);
-
-      possibleLine1.delta = possibleLine1.delta.map(it => it / 2);
-      possibleLine2.delta = possibleLine2.delta.map(it => it / 2);
-
-      let possible = [];
-
-      if (isIntegers(possibleLine1.delta) && isIntegers(possibleLine2.delta)) {
-        // console.log('ints');
-        // console.log(possibleLine1);
-        // console.log(possibleLine2);
-        possible.push(moveStartTo(possibleLine1, -1));
-        possible.push(moveStartTo(possibleLine1, -3));
-        possible.push(moveStartTo(possibleLine2, -1));
-        possible.push(moveStartTo(possibleLine2, -3));
-      }
-
-      if (possible.length === 0) continue;
-
-      if (i === 4 && j === 1) {
-        debugger
-      }
-
-      for (const pos of possible) {
-
-        let cnt = 0;
-        for (let k = 0; k < hails.length; k++) {
-          if (k === i || k === j) continue;
-
-          let xxx = intersect3(pos, hails[k]);
-
-          if (xxx) {
-            cnt++;
-            // console.log('WINNER?', pos);
-            // debugger
-            // if (isIntegers(Object.values(xxx))) {
-            // console.log('possible');
-            // console.log(pos);
-            // console.log(xxx, i,j, k, hails[k]);
-            // }
+      for (let t1 = 1; t1 < 200; t1++) {
+        for (let t2 = t1 + 1; t2 < 201; t2++) {
+          let ans = simulate(i, j, hails, t1, t2);
+          if (ans) {
+            return ans;
           }
         }
-        if (cnt === hails.length - 2) {
-          console.log('WINNER?', pos);
-          console.log({ cnt });
-          return  sum(pos.start)
-        }
-      };
-
-      // let inter = intersect3(possibleLine, hails[j])
-      // console.log(inter);
-
-      /*
-
-        x1 +m1*t = x2 + m2 * t;
-        x1 - x2 = m2 * t - m1 * t;
-        x1 - x2 = (m2 - m1) * t;
-        (x1 - x2) / (m2 - m1) =  t;
-
-      */
-
-      // console.log(pointInTime(possibleLine, -1), pointInTime(possibleLine, -3));
-
-      // if(isEqIntegers(eq)) {
-      //   console.log('found', eq);
-      // }
-
-      continue;
-
-      let cross = intersect(hail1, hail2);
-
-
-      let isInFuture1 = cross[0] >= hail1.start[0] && hail1.delta[0] > 0
-        || cross[0] <= hail1.start[0] && hail1.delta[0] < 0;
-      let isInFuture2 = cross[0] >= hail2.start[0] && hail2.delta[0] > 0
-        || cross[0] <= hail2.start[0] && hail2.delta[0] < 0;
-
-      // console.log(hail1, hail2);
-      if (Number.isFinite(cross[0]) && Number.isFinite(cross[1])) {
-        if (isInArea(area, cross)) {
-
-          if (isInFuture1 && isInFuture2) {
-            // console.log('inside future');
-            cnt++;
-          } else {
-            // console.log('inside past');
-          }
-        } else {
-          // console.log('outside');
-        }
-      } else {
-        // console.log('parallel');
       }
 
     }
   }
 
-  return 123;
+  return 'NOT FOUND';
 }
 
 function intersect(lineA, lineB) {
@@ -227,7 +139,7 @@ function intersect3(line1, line2) {
   let tY = (line1.start[1] - line2.start[1]) / (line2.delta[1] - line1.delta[1]);
   let tZ = (line1.start[2] - line2.start[2]) / (line2.delta[2] - line1.delta[2]);
 
-  let ts = [tX, tY, tZ].filter((x) => !Number.isNaN(x))
+  let ts = [tX, tY, tZ].filter((x) => !Number.isNaN(x));
 
   // console.log({ tX, tY, tZ });
   if (ts.every(it => it === ts[0])) {
@@ -277,4 +189,63 @@ function isEqIntegers(eq) {
 
 function isIntegers(coords) {
   return coords.every(it => Number.isInteger(it));
+}
+
+function simulate(i, j, hails, time1, time2) {
+  let point1 = pointInTime(hails[i], time1);
+  let point2 = pointInTime(hails[j], time2);
+
+  let possibleLine1 = makeLineByDots(point2, point1);
+  let possibleLine2 = makeLineByDots(point1, point2);
+
+
+  possibleLine1.delta = possibleLine1.delta.map(it => it / (time2 - time1));
+  possibleLine2.delta = possibleLine2.delta.map(it => it / (time2 - time1));
+
+  let possible = [];
+
+  if (isIntegers(possibleLine1.delta) && isIntegers(possibleLine2.delta)) {
+    // console.log('ints');
+    // console.log(possibleLine1);
+    // console.log(possibleLine2);
+    possible.push(moveStartTo(possibleLine1, -time1));
+    possible.push(moveStartTo(possibleLine1, -time2));
+    possible.push(moveStartTo(possibleLine2, -time1));
+    possible.push(moveStartTo(possibleLine2, -time2));
+  }
+
+  if (possible.length === 0) return null;
+
+
+  // if (i === 4 && j === 1) {
+  //   debugger
+  // }
+
+  for (const pos of possible) {
+
+    let cnt = 0;
+    for (let k = 0; k < hails.length; k++) {
+      if (k === i || k === j) continue;
+
+      let xxx = intersect3(pos, hails[k]);
+
+      if (xxx) {
+        cnt++;
+        // console.log('WINNER?', pos);
+        // debugger
+        // if (isIntegers(Object.values(xxx))) {
+        // console.log('possible');
+        // console.log(pos);
+        // console.log(xxx, i,j, k, hails[k]);
+        // }
+      }
+    }
+    if (cnt === hails.length - 2) {
+      // console.log('WINNER?', pos);
+      // console.log({ cnt });
+      return sum(pos.start);
+    }
+  }
+
+  return null;
 }
