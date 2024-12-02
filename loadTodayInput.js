dcb456ec5e65fcf629d7bff01f5c831fa6ffa55a
Joxter <joxterrr@gmail.com>
2024-12-02 21:54:13 +0100

    update loadTodayInput

--- loadTodayInput.js
+++ loadTodayInput.js
@@ -1,35 +1,38 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-let day = (new Date()).getDate();
-let year = (new Date()).getFullYear();
-let DD = String(day).padStart(2, '0');
+let day = new Date().getDate();
+let year = new Date().getFullYear();
+let DD = String(day).padStart(2, "0");
 
 let url = `https://adventofcode.com/${year}/day/${day}/input`;
 let token = process.env.SESSION_TOKEN;
 
 if (!token) {
-  console.error('SESSION_TOKEN env variable is not set');
+  console.error("SESSION_TOKEN env variable is not set");
   process.exit(1);
 }
 
-console.log('Loading...', url);
+console.log("Loading...", url);
 
 fetch(url, {
   headers: {
-    cookie: `session=${token}`
-  }
+    cookie: `session=${token}`,
+  },
 })
   .then((res) => res.text())
   .then((text) => {
-    console.log('Loaded');
+    console.log("Loaded");
 
     text = text.trim();
 
     let inputFolder = new URL(`./inputs/${year}`, import.meta.url).pathname;
+    let answersFolder = new URL(`./${year}/answers`, import.meta.url).pathname;
     fs.mkdirSync(inputFolder, { recursive: true });
     fs.writeFileSync(path.join(inputFolder, `day${DD}.txt`), text);
-    console.log('Saved!');
+    fs.writeFileSync(path.join(answersFolder, `day${DD}-part1.txt`), "");
+    fs.writeFileSync(path.join(answersFolder, `day${DD}-part2.txt`), "");
+    console.log("Saved!");
 
     createTodayJs(day);
     console.log(`day${DD}.js has been created, enjoy!`);
@@ -57,9 +60,7 @@ function part1(inp) {
 function part2(inp) {
   return 123;
 }
-`
-  ;
-
+`;
   let folder = new URL(`./${year}/js/`, import.meta.url).pathname;
   fs.mkdirSync(folder, { recursive: true });
   fs.writeFileSync(path.join(folder, `day${DD}.js`), content);
