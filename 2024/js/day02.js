750f9f5556ebc4ed92d0a61cce3d0d27cd9fe41f
Joxter <joxterrr@gmail.com>
2024-12-02 20:56:32 +0100

    2024 02 add better

--- 2024/js/day02.js
+++ 2024/js/day02.js
@@ -2,10 +2,11 @@ import { runDay } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/2
 
-runDay(2024, 2)
+runDay(2024, 2, 1)
   //
   .part(1, part1)
   .part(2, part2)
+  .part(2, part2better, "better")
   .end();
 
 function part1(inp) {
@@ -33,6 +34,27 @@ function part2(inp) {
   }).length;
 }
 
+function part2better(inp) {
+  let lines = inp
+    .trim()
+    .split("\n")
+    .map((it) => it.split(" ").map((it) => +it));
+
+  return lines.filter((level) => {
+    let normalRes = isSafeBetter(level);
+    if (normalRes === true) return true;
+    let noFirstRes = isSafeBetter(level.toSpliced(0, 1));
+    if (noFirstRes === true) return true;
+
+    let finalRes = isSafeBetter(
+      level.toSpliced(Math.max(normalRes, noFirstRes), 1),
+    );
+    if (finalRes === true) return true;
+
+    return false;
+  }).length;
+}
+
 function isSafe(level) {
   let mode = level[0] < level[1] ? "inc" : "dec";
 
@@ -48,5 +70,25 @@ function isSafe(level) {
       let prev = level[i - 1];
       if (it < prev && prev - it <= 3) return true;
     });
+  }
+}
+
+function isSafeBetter(level) {
+  if (level[0] < level.at(-1)) {
+    for (let i = 1; i < level.length; i++) {
+      let prev = level[i - 1];
+      if (!(level[i] > prev && level[i] - prev <= 3)) {
+        return i;
+      }
+    }
+    return true;
+  } else {
+    for (let i = 1; i < level.length; i++) {
+      let prev = level[i - 1];
+      if (!(level[i] < prev && prev - level[i] <= 3)) {
+        return i;
+      }
+    }
+    return true;
   }
 }
