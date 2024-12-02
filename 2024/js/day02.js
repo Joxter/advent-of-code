e1a68254173781f017fe079ec829ece0dad1d808
Joxter <joxterrr@gmail.com>
2024-12-02 20:23:40 +0100

    2024 02 clean up

--- 2024/js/day02.js
+++ 2024/js/day02.js
@@ -2,17 +2,11 @@ import { runDay } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/2
 
-console.log(
-  part2(`7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9`),
-  [2],
-);
-
-runDay(2024, 2).part(1, part1).part(2, part2).end();
+runDay(2024, 2)
+  //
+  .part(1, part1)
+  .part(2, part2)
+  .end();
 
 function part1(inp) {
   let lines = inp
@@ -20,23 +14,7 @@ function part1(inp) {
     .split("\n")
     .map((it) => it.split(" ").map((it) => +it));
 
-  return lines.filter((level) => {
-    let mode = level[0] < level[1] ? "inc" : "dec";
-
-    if (mode === "inc") {
-      return level.every((it, i) => {
-        if (i === 0) return true;
-        let prev = level[i - 1];
-        if (it > prev && it - prev <= 3) return true;
-      });
-    } else {
-      return level.every((it, i) => {
-        if (i === 0) return true;
-        let prev = level[i - 1];
-        if (it < prev && prev - it <= 3) return true;
-      });
-    }
-  }).length;
+  return lines.filter((level) => isSafe(level)).length;
 }
 
 function part2(inp) {
@@ -47,9 +25,7 @@ function part2(inp) {
 
   return lines.filter((level) => {
     for (let i = 0; i < level.length; i++) {
-      let l2 = level.slice();
-      l2.splice(i, 1);
-      if (isSafe(l2)) {
+      if (isSafe(level.toSpliced(i, 1))) {
         return true;
       }
     }
