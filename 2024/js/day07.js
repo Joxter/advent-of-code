b803f9d3acd47520e746cf18a5d714b17cbe4ea2
Joxter <joxterrr@gmail.com>
2024-12-07 14:39:36 +0100

    2024 7 add better

--- 2024/js/day07.js
+++ 2024/js/day07.js
@@ -2,46 +2,129 @@ import { runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/7
 
-runDay(2024, 7)
-  //
-  .part(1, part1)
-  .part(2, part2)
+runDay(2024, 7, 100)
+  .part(1, part1) // 0.350 (100 iters)
+  .part(1, part1rev, "rev") // 0.078 (100 iters)
+  .part(2, part2) // 22.557 (100 iters)
+  .part(2, part2rev, "rev") // 0.150 (100 iters)
   .end();
 
 function part1(inp) {
-  return sum(
-    inp
-      .trim()
-      .split("\n")
-      .map((line) => {
-        let [_total, _nums] = line.split(": ");
-
-        let total = +_total;
-        let nums = _nums.split(" ").map(Number);
-
-        let res = {};
-        trySum(total, nums, 1, nums[0], res);
-
-        return res.ok ? total : 0;
-      }),
-  );
-}
-
-function trySum(total, nums, i, sum, ref) {
-  if (ref.ok || sum > total) return;
-  if (i === nums.length) {
-    if (sum === total) {
-      ref.ok = true;
-    }
-    return;
-  }
-  let n = nums[i];
-
-  trySum(total, nums, i + 1, n + sum, ref);
-  trySum(total, nums, i + 1, n * sum, ref);
+  return parseAndRun(inp, trySum);
 }
 
 function part2(inp) {
+  return parseAndRun(inp, trySum2);
+}
+
+function part1rev(inp) {
+  return parseAndRun(inp, trySumReverse);
+}
+
+function part2rev(inp) {
+  return parseAndRun(inp, trySum2reverse);
+}
+
+function trySum(total, nums) {
+  let ok = false;
+  go(total, nums, 1, nums[0]);
+  return ok;
+
+  function go(total, nums, i, sum) {
+    if (sum > total || ok) return;
+    if (i === nums.length) {
+      if (sum === total) {
+        ok = true;
+      }
+      return;
+    }
+    let n = nums[i];
+
+    go(total, nums, i + 1, n + sum);
+    go(total, nums, i + 1, n * sum);
+  }
+}
+
+function trySum2(total, nums) {
+  let ok = false;
+  go(total, nums, 1, nums[0]);
+  return ok;
+
+  function go(total, nums, i, sum) {
+    if (sum > total || ok) return;
+    if (i === nums.length) {
+      if (sum === total) {
+        ok = true;
+      }
+      return;
+    }
+    let n = nums[i];
+
+    go(total, nums, i + 1, n + sum);
+    go(total, nums, i + 1, n * sum);
+    go(total, nums, i + 1, +`${sum}${n}`);
+  }
+}
+
+function trySumReverse(total, nums) {
+  let ok = false;
+  go(total, nums, nums.length - 1);
+  return ok;
+
+  function go(total, nums, i) {
+    if (ok) return;
+    if (total < 0) return;
+    if (total === 0 && i >= 0) return;
+    if (i < 0) {
+      if (total === 0) {
+        ok = true;
+      }
+      return;
+    }
+    let n = nums[i];
+
+    let a = total / n;
+    if (Number.isInteger(a)) {
+      go(a, nums, i - 1);
+    }
+    go(total - n, nums, i - 1);
+  }
+}
+
+function trySum2reverse(total, nums) {
+  let tens = [1, 10, 100, 1000];
+
+  let ok = false;
+  go(total, nums, nums.length - 1);
+  return ok;
+
+  function go(total, nums, i) {
+    if (ok) return;
+    if (total < 0) return;
+    if (total === 0 && i >= 0) return;
+    if (i < 0) {
+      if (total === 0) {
+        ok = true;
+      }
+      return;
+    }
+    let n = nums[i];
+
+    go(total - n, nums, i - 1);
+
+    let a = total / n;
+    if (Number.isInteger(a)) {
+      go(a, nums, i - 1);
+    }
+
+    let b = (total - n) / tens[Math.floor(Math.log10(n)) + 1];
+    if (Number.isInteger(b)) {
+      go(b, nums, i - 1);
+    }
+  }
+}
+
+function parseAndRun(inp, fn) {
   return sum(
     inp
       .trim()
@@ -52,25 +135,7 @@ function part2(inp) {
         let total = +_total;
         let nums = _nums.split(" ").map(Number);
 
-        let res = {};
-        trySum2(total, nums, 1, nums[0], res);
-
-        return res.ok ? total : 0;
+        return fn(total, nums) ? total : 0;
       }),
   );
-}
-
-function trySum2(total, nums, i, sum, ref) {
-  if (ref.ok || sum > total) return;
-  if (i === nums.length) {
-    if (sum === total) {
-      ref.ok = true;
-    }
-    return;
-  }
-  let n = nums[i];
-
-  trySum2(total, nums, i + 1, n + sum, ref);
-  trySum2(total, nums, i + 1, n * sum, ref);
-  trySum2(total, nums, i + 1, +`${sum}${n}`, ref);
 }
