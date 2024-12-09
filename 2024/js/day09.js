112af01d35f37457112cd88ab2546cec3e7a2043
Joxter <joxterrr@gmail.com>
2024-12-09 07:23:21 +0100

    2024 9 p2 wip

--- 2024/js/day09.js
+++ 2024/js/day09.js
@@ -2,12 +2,11 @@ import { runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/9
 
-console.log(part1(`2333133121414131402`), [1928]);
-// console.log(part1(`12345`));
+console.log(part2(`2333133121414131402`), [2858]);
 
 runDay(2024, 9)
   //
-  .part(1, part1)
+  // .part(1, part1) // [6395800119709]
   // .part(2, part2)
   .end();
 
@@ -31,10 +30,8 @@ function part1(inp) {
 
   while (disk.includes(".")) {
     let ind = disk.indexOf(".");
-
     disk[ind] = disk.pop();
   }
-  // console.log(disk.join(""));
 
   return sum(
     disk.map((n, i) => {
@@ -44,5 +41,31 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let disk = [];
+
+  let file = true;
+  let id = 0;
+  inp
+    .trim()
+    .split("")
+    .forEach((n) => {
+      if (file) {
+        disk.push([+n,String(id)]);
+        id++;
+      } else {
+        disk.push([+n,'.']);
+      }
+      file = !file;
+    });
+
+  while (disk.includes(".")) {
+    let ind = disk.indexOf(".");
+    disk[ind] = disk.pop();
+  }
+
+  return sum(
+    disk.map((n, i) => {
+      return n * i;
+    }),
+  );
 }
