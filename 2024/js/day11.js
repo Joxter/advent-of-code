7b8f7da1ef4cb3adc25cc993dacabdf3495e5d25
Joxter <joxterrr@gmail.com>
2024-12-11 20:46:00 +0100

    2024 11 p2 alter

--- 2024/js/day11.js
+++ 2024/js/day11.js
@@ -2,10 +2,10 @@ import { runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/11
 
-runDay(2024, 11)
-  //
+runDay(2024, 11, 1)
   .part(1, part1)
   .part(2, part2)
+  .part(2, part2alter, "alter")
   .part(2, part2notMy, "not my (recursion + cache)")
   .end();
 
@@ -58,6 +58,42 @@ function part2(inp) {
   }
 
   return sum(Object.values(stones));
+}
+
+function part2alter(inp) {
+  let stones = Object.fromEntries(inp.split(" ").map((it) => [it, [1]]));
+  stones["1"] = [];
+
+  const N = 75;
+
+  for (let i = 0; i < N; i++) {
+    Object.entries(stones).forEach(([stone, cnts]) => {
+      let cnt = cnts[i];
+      if (!cnt) return;
+
+      if (stone.length % 2 === 0) {
+        let a = +stone.slice(0, stone.length / 2);
+        let b = +stone.slice(stone.length / 2);
+
+        if (!stones[a]) stones[a] = [];
+        if (!stones[b]) stones[b] = [];
+
+        stones[a][i + 1] = (stones[a][i + 1] || 0) + cnt;
+        stones[b][i + 1] = (stones[b][i + 1] || 0) + cnt;
+      } else {
+        if (stone === "0") {
+          stones["1"][i + 1] = (stones["1"][i + 1] || 0) + cnt;
+        } else {
+          let nn = stone * 2024;
+
+          if (!stones[nn]) stones[nn] = [];
+          stones[nn][i + 1] = (stones[nn][i + 1] || 0) + cnt;
+        }
+      }
+    });
+  }
+
+  return sum(Object.values(stones).map((cnts) => cnts[N] || 0));
 }
 
 function part2notMy(inp) {
