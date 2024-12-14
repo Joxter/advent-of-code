b53ea68e474ff93b00e4367acdbbfd9a037a0901
Joxter <joxterrr@gmail.com>
2024-12-14 09:01:34 +0100

    2024 14 p2

--- 2024/js/day14.js
+++ 2024/js/day14.js
@@ -2,30 +2,10 @@ import { ints, printGrid, runDay } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/14
 
-console.log(
-  part1(`p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3`),
-  [12],
-);
-
-// console.log(
-//   part1(`p=2,4 v=2,-3`),
-// );
-
 runDay(2024, 14)
   //
   .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -56,7 +36,7 @@ function part1(inp) {
     // console.log(printMap(robots));
   }
 
-  console.log(printMap(robots));
+  // console.log(printMap(robots));
 
   let q1 = 0;
   let q2 = 0;
@@ -71,13 +51,9 @@ function part1(inp) {
   // 7/2 - 1 = 2
   // 7/2 + 1 = 4
 
-  // 11 = 01234 67890
-  let arr = "11111022222".split("");
-  // 01234567890
-  // console.log(Math.floor(map[0] / 2), Math.ceil(map[0] / 2));
   let ii = Math.floor(map[0] / 2);
   let jj = Math.ceil(map[1] / 2) - 1;
-  console.log([ii, jj], [map[0], map[1]], [5, 3]); // 11 / 7
+  // console.log([ii, jj], [map[0], map[1]], [5, 3]); // 11 / 7
   // console.log(arr[ii], arr[jj]);
 
   // [ 53, 50 ] [ 107, 101 ] [ 5, 3 ]
@@ -85,24 +61,24 @@ function part1(inp) {
   // console.log(robots);
   robots.forEach(({ pos }) => {
     let [i, j] = pos;
-    console.log([i, j]);
+    // console.log([i, j]);
     if (i < ii && j < jj) {
-      console.log("q1");
+      // console.log("q1");
       q1 += 1;
     } else if (i > ii && j < jj) {
-      console.log("q2");
+      // console.log("q2");
       q2 += 1;
     } else if (i > ii && j > jj) {
-      console.log("q4");
+      // console.log("q4");
       q4 += 1;
     } else if (i < ii && j > jj) {
-      console.log("q3");
+      // console.log("q3");
       q3 += 1;
     } else {
-      console.log("no");
+      // console.log("no");
     }
   });
-  console.log([q1, q2, q3, q4]);
+  // console.log([q1, q2, q3, q4]);
 
   return q1 * q2 * q3 * q4;
 
@@ -120,5 +96,101 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let robots = inp.split("\n").map((line) => {
+    let [a, b, c, d] = ints(line);
+
+    return { pos: [a, b], v: [c, d] };
+  });
+
+  // let map = [11, 7]; // width, height
+  let map = [101, 103];
+
+  let initPosition = robots.map((it) => it.pos.join(",")).join(",");
+
+  // console.log("INIT");
+  // console.log(printMap(robots));
+
+  // loop = 10403
+  let rects = {};
+  let mm = {};
+  for (let i = 1; i <= 10_503; i++) {
+    robots.forEach(({ pos, v }, i) => {
+      //
+      let newPos = [
+        //
+        (map[0] + pos[0] + v[0]) % map[0],
+        (map[1] + pos[1] + v[1]) % map[1],
+      ];
+      // console.log(pos, newPos);
+      robots[i].pos = newPos;
+    });
+
+    let r = rect(robots);
+    if (!rects[r]) rects[r] = 0;
+    rects[r]++;
+
+    let a = maxCnt(robots);
+    if (!mm[a]) mm[a] = 0;
+    mm[a]++;
+
+    // if (r.join(",") === "99,102") {
+    //   console.log(printMap(robots));
+    //   console.log(i);
+    // }
+    if (a === 1) {
+      // console.log(printMap(robots));
+      // console.log(i);
+      return i;
+    }
+
+    if (initPosition === robots.map((it) => it.pos.join(",")).join(",")) {
+      // console.log("LOOP!", i);
+      break;
+    }
+
+    // console.log("after " + i);
+    // console.log(printMap(robots));
+  }
+  // console.log(mm);
+  // console.log(rects);
+
+  function maxCnt(robots) {
+    let max = {};
+    robots.forEach(({ pos }) => {
+      if (!max[pos]) max[pos] = 0;
+      max[pos]++;
+    });
+
+    return Math.max(...Object.values(max));
+  }
+
+  function rect(robots) {
+    let a = [0, 0];
+    let b = [...map];
+    robots.forEach(({ pos }) => {
+      a[0] = Math.max(a[0], pos[0]);
+      a[1] = Math.max(a[1], pos[1]);
+      b[0] = Math.min(b[0], pos[0]);
+      b[1] = Math.min(b[1], pos[1]);
+    });
+    return [a[0] - b[0], a[1] - b[1]];
+  }
+
+  console.log("end");
+
+  // console.log(printMap(robots));
+
+  return 4;
+
+  function printMap(robots) {
+    let res = Array(map[1])
+      .fill(0)
+      .map(() => Array(map[0]).fill("0"));
+
+    robots.forEach(({ pos }) => {
+      res[pos[1]][pos[0]]++;
+    });
+
+    return printGrid(res);
+  }
 }
