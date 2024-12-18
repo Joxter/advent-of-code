05a35960b796890abb1ef545d0c026d9b4cabbc2
Joxter <joxterrr@gmail.com>
2024-12-18 08:01:15 +0100

    2024 18 p2

--- 2024/js/day18.js
+++ 2024/js/day18.js
@@ -2,45 +2,17 @@ import { printGrid, runDay } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/18
 
-// console.log(
-//   part1(`5,4
-// 4,2
-// 4,5
-// 3,0
-// 2,1
-// 6,3
-// 2,4
-// 1,5
-// 0,6
-// 3,3
-// 2,6
-// 5,1
-// 1,2
-// 5,5
-// 2,5
-// 6,5
-// 1,4
-// 0,4
-// 6,4
-// 1,1
-// 6,1
-// 1,0
-// 0,5
-// 1,6
-// 2,0`),
-// );
-
 runDay(2024, 18)
   //
-  .part(1, part1)
-  // .part(2, part2)
+  // .part(1, part1)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
-  // let size = 6;
-  // let bytes = 12;
-  let size = 70;
-  let bytes = 1024;
+  let size = 6;
+  let bytes = 12;
+  // let size = 70;
+  // let bytes = 1024;
 
   let grid = Array(size + 1)
     .fill(".")
@@ -58,6 +30,7 @@ function part1(inp) {
       // console.log(i);
       // console.log(printGrid(grid));
     });
+  console.log(printGrid(grid));
 
   let q = [[[0, 0], 0]];
 
@@ -67,7 +40,7 @@ function part1(inp) {
     //   continue;
     // }
 
-    if (grid[y]?.[x] === '.') {
+    if (grid[y]?.[x] === ".") {
       grid[y][x] = len;
       q.push([[x + 1, y], len + 1]);
       q.push([[x - 1, y], len + 1]);
@@ -83,5 +56,60 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  // return 123;
+  // let size = 6;
+  // let bytes = 12 + 100;
+  let size = 70;
+  let bytes = 1024222;
+
+  let rawBytes = inp.trim().split("\n");
+
+  for (let b = 1; b < rawBytes.length; b++) {
+    let grid = Array(size + 1)
+      .fill(".")
+      .map(() => {
+        return Array(size + 1).fill(".");
+      });
+
+    inp
+      .trim()
+      .split("\n")
+      .slice(0, b)
+      .forEach((line, i) => {
+        // if (b > i) return;
+        let [x, y] = line.split(",");
+        grid[+y][+x] = "#";
+      });
+
+    // console.log(rawBytes[skip]);
+    // console.log(printGrid(grid));
+    // return;
+
+    /*
+2936 18,62 500 --- Correct!
+2937 40,58 . ()
+    */
+    // console.log(b, rawBytes[b], find(grid));
+    if (typeof find(grid) !== "number") {
+      return rawBytes[b];
+    }
+  }
+
+  function find(grid) {
+    let q = [[[0, 0], 0]];
+
+    while (q.length > 0) {
+      let [[x, y], len] = q.shift();
+      if (grid[y]?.[x] === ".") {
+        grid[y][x] = len;
+        q.push([[x + 1, y], len + 1]);
+        q.push([[x - 1, y], len + 1]);
+        q.push([[x, y + 1], len + 1]);
+        q.push([[x, y - 1], len + 1]);
+      }
+    }
+
+    return grid[size][size];
+  }
+
+  return "none";
 }
