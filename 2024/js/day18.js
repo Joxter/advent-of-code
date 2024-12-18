9c6754fb00437232f6fa74bb95039ed60c83b860
Joxter <joxterrr@gmail.com>
2024-12-18 08:25:08 +0100

    2024 18 clean up

--- 2024/js/day18.js
+++ 2024/js/day18.js
@@ -4,21 +4,17 @@ import { printGrid, runDay } from "../../utils.js";
 
 runDay(2024, 18)
   //
-  // .part(1, part1)
+  .part(1, part1)
   .part(2, part2)
   .end();
 
 function part1(inp) {
-  let size = 6;
-  let bytes = 12;
-  // let size = 70;
-  // let bytes = 1024;
+  let size = 70;
+  let bytes = 1024;
 
   let grid = Array(size + 1)
     .fill(".")
-    .map(() => {
-      return Array(size + 1).fill(".");
-    });
+    .map(() => Array(size + 1).fill("."));
 
   inp
     .trim()
@@ -27,19 +23,12 @@ function part1(inp) {
     .forEach((line, i) => {
       let [x, y] = line.split(",");
       grid[+y][+x] = "#";
-      // console.log(i);
-      // console.log(printGrid(grid));
     });
-  console.log(printGrid(grid));
 
   let q = [[[0, 0], 0]];
 
   while (q.length > 0) {
     let [[x, y], len] = q.shift();
-    // if (x < 0 || x >= size || y < 0 || y >= size || grid[y][x] !== "#") {
-    //   continue;
-    // }
-
     if (grid[y]?.[x] === ".") {
       grid[y][x] = len;
       q.push([[x + 1, y], len + 1]);
@@ -49,49 +38,26 @@ function part1(inp) {
     }
   }
 
-  // console.log(printGrid(grid));
-  // console.log(grid);
-
-  return grid[size][size];
+  return grid.at(-1).at(-1);
 }
 
 function part2(inp) {
-  // let size = 6;
-  // let bytes = 12 + 100;
   let size = 70;
-  let bytes = 1024222;
+  let bytes = inp
+    .trim()
+    .split("\n")
+    .map((l) => l.split(","));
 
-  let rawBytes = inp.trim().split("\n");
-
-  for (let b = 1; b < rawBytes.length; b++) {
+  for (let b = bytes.length; b > 0; b--) {
     let grid = Array(size + 1)
       .fill(".")
-      .map(() => {
-        return Array(size + 1).fill(".");
-      });
+      .map(() => Array(size + 1).fill("."));
 
-    inp
-      .trim()
-      .split("\n")
-      .slice(0, b)
-      .forEach((line, i) => {
-        // if (b > i) return;
-        let [x, y] = line.split(",");
-        grid[+y][+x] = "#";
-      });
+    bytes.slice(0, b).forEach(([x, y]) => {
+      grid[+y][+x] = "#";
+    });
 
-    // console.log(rawBytes[skip]);
-    // console.log(printGrid(grid));
-    // return;
-
-    /*
-2936 18,62 500 --- Correct!
-2937 40,58 . ()
-    */
-    // console.log(b, rawBytes[b], find(grid));
-    if (typeof find(grid) !== "number") {
-      return rawBytes[b];
-    }
+    if (find(grid) !== ".") return bytes[b];
   }
 
   function find(grid) {
@@ -108,7 +74,7 @@ function part2(inp) {
       }
     }
 
-    return grid[size][size];
+    return grid.at(-1).at(-1);
   }
 
   return "none";
