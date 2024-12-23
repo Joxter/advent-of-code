730cbcf44f91b382c65009e334bcef93bf1ea328
Joxter <joxterrr@gmail.com>
2024-12-23 23:23:13 +0100

    2024 21 p2 fun

--- 2024/js/day21.js
+++ 2024/js/day21.js
@@ -251,32 +251,21 @@ function toPairs(code) {
 
 function part2notMy(inp) {
   // https://old.reddit.com/r/adventofcode/comments/1hj2odw/2024_day_21_solutions/m36z9y2/
-  const getSum = (a, b) => a + b;
-  const toInt = (n) => parseInt(n, 10);
 
-  class RobotInstructionTranslator {
-    #instructions;
+  let instructions = JSON.parse(
+    `{"10":">vA","12":">A","13":">>A","14":"^A","15":"^>A","16":"^>>A","17":"^^A","18":"^^>A","19":"^^>>A","20":"vA","21":"<A","23":">A","24":"<^A","25":"^A","26":"^>A","27":"<^^A","28":"^^A","29":"^^>A","30":"<vA","31":"<<A","32":"<A","34":"<<^A","35":"<^A","36":"^A","37":"<<^^A","38":"<^^A","39":"^^A","40":">vvA","41":"vA","42":"v>A","43":"v>>A","45":">A","46":">>A","47":"^A","48":"^>A","49":"^>>A","50":"vvA","51":"<vA","52":"vA","53":"v>A","54":"<A","56":">A","57":"<^A","58":"^A","59":"^>A","60":"<vvA","61":"<<vA","62":"<vA","63":"vA","64":"<<A","65":"<A","67":"<<^A","68":"<^A","69":"^A","70":">vvvA","71":"vvA","72":"vv>A","73":"vv>>A","74":"vA","75":"v>A","76":"v>>A","78":">A","79":">>A","80":"vvvA","81":"<vvA","82":"vvA","83":"vv>A","84":"<vA","85":"vA","86":"v>A","87":"<A","89":">A","90":"<vvvA","91":"<<vvA","92":"<vvA","93":"vvA","94":"<<vA","95":"<vA","96":"vA","97":"<<A","98":"<A","A0":"<A","A1":"^<<A","A2":"<^A","A3":"^A","A4":"^^<<A","A5":"<^^A","A6":"^^A","A7":"^^^<<A","A8":"<^^^A","A9":"^^^A","0A":">A","01":"^<A","02":"^A","03":"^>A","04":"^^<A","05":"^^A","06":"^^>A","07":"^^^<A","08":"^^^A","09":"^^^>A","1A":">>vA","2A":"v>A","3A":"vA","4A":">>vvA","5A":"vv>A","6A":"vvA","7A":">>vvvA","8A":">vvvA","9A":"vvvA","A^":"<A","A<":"v<<A","Av":"<vA","A>":"vA","^A":">A","^<":"v<A","^v":"vA","^>":"v>A","<A":">>^A","<^":">^A","<v":">A","<>":">>A","vA":"^>A","v^":"^A","v<":"<A","v>":">A",">A":"^A",">^":"<^A","><":"<<A",">v":"<A"}`,
+  );
 
-    constructor() {
-      // Priority: <, v, ^, >, A (except when it would make us cross over a gap)
-      this.#instructions = JSON.parse(
-        `{"10":">vA","12":">A","13":">>A","14":"^A","15":"^>A","16":"^>>A","17":"^^A","18":"^^>A","19":"^^>>A","20":"vA","21":"<A","23":">A","24":"<^A","25":"^A","26":"^>A","27":"<^^A","28":"^^A","29":"^^>A","30":"<vA","31":"<<A","32":"<A","34":"<<^A","35":"<^A","36":"^A","37":"<<^^A","38":"<^^A","39":"^^A","40":">vvA","41":"vA","42":"v>A","43":"v>>A","45":">A","46":">>A","47":"^A","48":"^>A","49":"^>>A","50":"vvA","51":"<vA","52":"vA","53":"v>A","54":"<A","56":">A","57":"<^A","58":"^A","59":"^>A","60":"<vvA","61":"<<vA","62":"<vA","63":"vA","64":"<<A","65":"<A","67":"<<^A","68":"<^A","69":"^A","70":">vvvA","71":"vvA","72":"vv>A","73":"vv>>A","74":"vA","75":"v>A","76":"v>>A","78":">A","79":">>A","80":"vvvA","81":"<vvA","82":"vvA","83":"vv>A","84":"<vA","85":"vA","86":"v>A","87":"<A","89":">A","90":"<vvvA","91":"<<vvA","92":"<vvA","93":"vvA","94":"<<vA","95":"<vA","96":"vA","97":"<<A","98":"<A","A0":"<A","A1":"^<<A","A2":"<^A","A3":"^A","A4":"^^<<A","A5":"<^^A","A6":"^^A","A7":"^^^<<A","A8":"<^^^A","A9":"^^^A","0A":">A","01":"^<A","02":"^A","03":"^>A","04":"^^<A","05":"^^A","06":"^^>A","07":"^^^<A","08":"^^^A","09":"^^^>A","1A":">>vA","2A":"v>A","3A":"vA","4A":">>vvA","5A":"vv>A","6A":"vvA","7A":">>vvvA","8A":">vvvA","9A":"vvvA","A^":"<A","A<":"v<<A","Av":"<vA","A>":"vA","^A":">A","^<":"v<A","^v":"vA","^>":"v>A","<A":">>^A","<^":">^A","<v":">A","<>":">>A","vA":"^>A","v^":"^A","v<":"<A","v>":">A",">A":"^A",">^":"<^A","><":"<<A",">v":"<A"}`,
-      );
-    }
-
-    translate(key) {
-      if (key[0] === key[1]) {
-        // Robot arms don't need to move in this case; we can just press 'A' again.
-        return "A";
-      } else {
-        return this.#instructions[key];
-      }
+  function translateInstruction(key) {
+    if (key[0] === key[1]) {
+      return "A";
+    } else {
+      return instructions[key];
     }
   }
 
   class RobotKeypad {
     #code;
-    #translator;
     #instructions;
 
     #setNewKeystrokes(counts, keyStrokes, multiplier) {
@@ -299,7 +288,7 @@ function part2notMy(inp) {
         // All robots start on "A" from the previous keystrokes, so we need to account for that.
         this.#setNewKeystrokes(
           commandCounts,
-          "A" + this.#translator.translate(instruction),
+          "A" + translateInstruction(instruction),
           count,
         );
       }
@@ -312,7 +301,7 @@ function part2notMy(inp) {
       for (let i = 0; i < instructions.length - 1; i++) {
         this.#setNewKeystrokes(
           commandCounts,
-          "A" + this.#translator.translate(instructions.substring(i, i + 2)),
+          "A" + translateInstruction(instructions.substring(i, i + 2)),
         );
       }
       for (; layers > 1; layers--) {
@@ -321,9 +310,8 @@ function part2notMy(inp) {
       return commandCounts;
     }
 
-    constructor(code, translator, layers) {
+    constructor(code, layers) {
       this.#code = code;
-      this.#translator = translator;
       this.#instructions = this.#translateMainKeypadInstructions(layers);
     }
 
@@ -331,15 +319,14 @@ function part2notMy(inp) {
       // The sum of these values gives us the number of movements the last robot needed to make.
       // Because all robots start at "A", this conveniently equals the number of key presses the
       // human has to make, so we don't need to adjust the resulting sum.
-      return this.#instructions.values().reduce(getSum, 0) * toInt(this.#code);
+      return sum(this.#instructions.values()) * +this.#code.slice(0, 3);
     }
   }
 
   function getCodeComplexities(input, layers) {
-    let translator = new RobotInstructionTranslator();
     let total = 0;
     for (let line of input) {
-      let keypad = new RobotKeypad(line, translator, layers);
+      let keypad = new RobotKeypad(line, layers);
       total += keypad.complexity;
     }
     return total;
