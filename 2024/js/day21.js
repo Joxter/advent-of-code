90b588a78b9293a0c6e624c53f2e8a3ced3591bc
Joxter <joxterrr@gmail.com>
2024-12-23 23:16:04 +0100

    2024 21 shame

--- 2024/js/day21.js
+++ 2024/js/day21.js
@@ -2,21 +2,11 @@ import { findInGrid, makeGrid, runDay, sum, uniq } from "../../utils.js";
 
 // https://adventofcode.com/2024/day/21
 
-// console.log(part1(`456A`));
-// console.log(part2(`029A`));
-// console.log(
-//   part1(`029A
-// 980A
-// 179A
-// 456A
-// 379A`),
-//   [126384],
-// );
-
 runDay(2024, 21)
   //
-  // .part(1, part1)
-  .part(2, part2) // high 638563429049266400
+  .part(1, part1)
+  // .part(2, part2) // not solved:(
+  .part(2, part2notMy)
   .end();
 
 function part1(inp) {
@@ -140,7 +130,7 @@ function part2(inp) {
     ...arrPaths,
     //
     "A<": ["v<<"],
-    ">^": ["^<"],
+    ">^": ["<^"],
     "^>": ["v>"],
     "<A": [">>^"],
     vA: [">^"],
@@ -148,50 +138,33 @@ function part2(inp) {
   };
 
   let complexity = codes.map((code) => {
-    console.log(code);
-    let p1 = getPath1("A" + code, numpadPaths);
+    let p1 = getPath1("A" + code, numpadPaths).slice(0, 1);
 
     let last = p1.map((p) => {
       let memo = {};
 
       let res = getPathNew("A" + p, 2, memo);
-      // console.log(memo);
       return res;
     });
 
-    // console.log(memo);
-    console.log(last);
-
-    // let min = Math.min(...last.map((p) => p.length));
-    /*
-
-    (2 steps) min
-    286A 68
-    480A 74
-    140A 70
-    413A 70
-    964A 72
-    */
     return +code.slice(0, 3) * 123;
   });
 
   return sum(complexity);
 
   function getPathNew(code, n, memo) {
-    if (n === 0) return code.length;
+    if (n === 0) return code + "A";
     if (memo[code + ":" + n]) return memo[code + ":" + n];
 
-    let p = 0;
+    let p = "";
 
     for (let i = 0; i < code.length - 1; i++) {
       let pair = code.slice(i, i + 2);
-      let possiblePath = arrPaths[pair][0];
-
-      p += getPathNew(possiblePath + "A", n - 1, memo);
+      let next = getPathOne("A" + pair, arrPaths);
+      p += getPathNew(next, n - 1, memo);
     }
 
     memo[code + ":" + n] = p;
-
     return p;
   }
 }
@@ -251,7 +224,7 @@ function getPath1(code, paths) {
   return p;
 }
 
-function getPathOne(code, paths, n) {
+function getPathOne(code, paths) {
   let p = "";
 
   for (let i = 0; i < code.length - 1; i++) {
@@ -274,4 +247,103 @@ function toPairs(code) {
   }
 
   return p;
+}
+
+function part2notMy(inp) {
+  // https://old.reddit.com/r/adventofcode/comments/1hj2odw/2024_day_21_solutions/m36z9y2/
+  const getSum = (a, b) => a + b;
+  const toInt = (n) => parseInt(n, 10);
+
+  class RobotInstructionTranslator {
+    #instructions;
+
+    constructor() {
+      // Priority: <, v, ^, >, A (except when it would make us cross over a gap)
+      this.#instructions = JSON.parse(
+        `{"10":">vA","12":">A","13":">>A","14":"^A","15":"^>A","16":"^>>A","17":"^^A","18":"^^>A","19":"^^>>A","20":"vA","21":"<A","23":">A","24":"<^A","25":"^A","26":"^>A","27":"<^^A","28":"^^A","29":"^^>A","30":"<vA","31":"<<A","32":"<A","34":"<<^A","35":"<^A","36":"^A","37":"<<^^A","38":"<^^A","39":"^^A","40":">vvA","41":"vA","42":"v>A","43":"v>>A","45":">A","46":">>A","47":"^A","48":"^>A","49":"^>>A","50":"vvA","51":"<vA","52":"vA","53":"v>A","54":"<A","56":">A","57":"<^A","58":"^A","59":"^>A","60":"<vvA","61":"<<vA","62":"<vA","63":"vA","64":"<<A","65":"<A","67":"<<^A","68":"<^A","69":"^A","70":">vvvA","71":"vvA","72":"vv>A","73":"vv>>A","74":"vA","75":"v>A","76":"v>>A","78":">A","79":">>A","80":"vvvA","81":"<vvA","82":"vvA","83":"vv>A","84":"<vA","85":"vA","86":"v>A","87":"<A","89":">A","90":"<vvvA","91":"<<vvA","92":"<vvA","93":"vvA","94":"<<vA","95":"<vA","96":"vA","97":"<<A","98":"<A","A0":"<A","A1":"^<<A","A2":"<^A","A3":"^A","A4":"^^<<A","A5":"<^^A","A6":"^^A","A7":"^^^<<A","A8":"<^^^A","A9":"^^^A","0A":">A","01":"^<A","02":"^A","03":"^>A","04":"^^<A","05":"^^A","06":"^^>A","07":"^^^<A","08":"^^^A","09":"^^^>A","1A":">>vA","2A":"v>A","3A":"vA","4A":">>vvA","5A":"vv>A","6A":"vvA","7A":">>vvvA","8A":">vvvA","9A":"vvvA","A^":"<A","A<":"v<<A","Av":"<vA","A>":"vA","^A":">A","^<":"v<A","^v":"vA","^>":"v>A","<A":">>^A","<^":">^A","<v":">A","<>":">>A","vA":"^>A","v^":"^A","v<":"<A","v>":">A",">A":"^A",">^":"<^A","><":"<<A",">v":"<A"}`,
+      );
+    }
+
+    translate(key) {
+      if (key[0] === key[1]) {
+        // Robot arms don't need to move in this case; we can just press 'A' again.
+        return "A";
+      } else {
+        return this.#instructions[key];
+      }
+    }
+  }
+
+  class RobotKeypad {
+    #code;
+    #translator;
+    #instructions;
+
+    #setNewKeystrokes(counts, keyStrokes, multiplier) {
+      multiplier = multiplier || 1;
+      for (let j = 0; j < keyStrokes.length - 1; j++) {
+        // The order of the movements doesn't matter, just how many times each one happens.
+        let key = keyStrokes.substring(j, j + 2);
+        if (counts.has(key)) {
+          counts.set(key, counts.get(key) + multiplier);
+        } else {
+          counts.set(key, multiplier);
+        }
+      }
+    }
+
+    #translateParentInstructions(instructions) {
+      let commandCounts = new Map();
+      for (let instruction of instructions.keys()) {
+        let count = instructions.get(instruction);
+        // All robots start on "A" from the previous keystrokes, so we need to account for that.
+        this.#setNewKeystrokes(
+          commandCounts,
+          "A" + this.#translator.translate(instruction),
+          count,
+        );
+      }
+      return commandCounts;
+    }
+
+    #translateMainKeypadInstructions(layers) {
+      let instructions = "A" + this.#code;
+      let commandCounts = new Map();
+      for (let i = 0; i < instructions.length - 1; i++) {
+        this.#setNewKeystrokes(
+          commandCounts,
+          "A" + this.#translator.translate(instructions.substring(i, i + 2)),
+        );
+      }
+      for (; layers > 1; layers--) {
+        commandCounts = this.#translateParentInstructions(commandCounts);
+      }
+      return commandCounts;
+    }
+
+    constructor(code, translator, layers) {
+      this.#code = code;
+      this.#translator = translator;
+      this.#instructions = this.#translateMainKeypadInstructions(layers);
+    }
+
+    get complexity() {
+      // The sum of these values gives us the number of movements the last robot needed to make.
+      // Because all robots start at "A", this conveniently equals the number of key presses the
+      // human has to make, so we don't need to adjust the resulting sum.
+      return this.#instructions.values().reduce(getSum, 0) * toInt(this.#code);
+    }
+  }
+
+  function getCodeComplexities(input, layers) {
+    let translator = new RobotInstructionTranslator();
+    let total = 0;
+    for (let line of input) {
+      let keypad = new RobotKeypad(line, translator, layers);
+      total += keypad.complexity;
+    }
+    return total;
+  }
+
+  return getCodeComplexities(inp.split("\n"), 26);
 }
