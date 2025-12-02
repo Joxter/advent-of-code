55a86f82dfb6aaadbabb57b7b2b452f68103493c
Joxter <joxterrr@gmail.com>
2025-12-02 20:17:41 +0100

    2025 2 fun

--- 2025/js/day02.js
+++ 2025/js/day02.js
@@ -8,6 +8,7 @@ runDay(2025, 2, 10)
   .part(2, part2)
   .part(1, part1regexp, 'regexp')
   .part(2, part2regexp, 'regexp')
+  .part(1, part1fun, 'fun')
   .end();
 
 function part1(inp) {
@@ -85,3 +86,27 @@ function part2regexp(inp) {
   return sum(ids);
 }
 
+function part1fun(inp) {
+  let ids = [];
+
+  inp.split(",").forEach((range) => {
+    let [from, to] = range.split("-");
+    from = +from;
+    to = +to;
+
+    for (let i = from; i <= to; i++) {
+      let str = String(i);
+
+      if (str.length % 2 === 1) {
+        i = +"9".repeat(i.toString().length) + 1;
+      } else {
+        if (str.slice(0, str.length / 2) === str.slice(str.length / 2)) {
+          ids.push(i);
+          i += +"9".repeat(str.length / 2) + 1;
+        }
+      }
+    }
+  });
+
+  return sum(ids);
+}
