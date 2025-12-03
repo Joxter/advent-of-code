46766a7c30a2416b46b92b22a0f5c28cfb328233
Joxter <joxterrr@gmail.com>
2025-12-03 07:26:25 +0100

    2025 3 p2

--- 2025/js/day03.js
+++ 2025/js/day03.js
@@ -2,17 +2,10 @@ import { runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2025/day/3
 
-console.log(
-  part1(`987654321111111
-811111111111119
-234234234234278
-818181911112111`),
-);
-
 runDay(2025, 3)
   //
   .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -22,7 +15,6 @@ function part1(inp) {
     let index = -1;
     let max = 0;
     for (let i = from; i < to; i++) {
-      let item = bank[i];
       if (bank[i] > max) {
         max = bank[i];
         index = i;
@@ -36,11 +28,40 @@ function part1(inp) {
     banks.map((bank) => {
       let [ai, a] = findMax(bank, 0, bank.length - 1);
 
-      return a * 10 + findMax(bank, ai+ 1, bank.length)[1];
+      return a * 10 + findMax(bank, ai + 1, bank.length)[1];
     }),
   );
 }
 
 function part2(inp) {
-  return 123;
+  let banks = inp.split("\n").map((b) => b.split("").map((j) => +j));
+
+  function findMax(bank, from, to) {
+    let index = -1;
+    let max = 0;
+    for (let i = from; i < to; i++) {
+      if (bank[i] > max) {
+        max = bank[i];
+        index = i;
+      }
+    }
+
+    return [index, max];
+  }
+
+  return sum(
+    banks.map((bank) => {
+      let joltage = 0;
+      let from = 0;
+      let to = bank.length - 12;
+
+      for (let i = 1; i <= 12; i++) {
+        let [ai, a] = findMax(bank, from, to + i);
+        from = ai + 1;
+        joltage = joltage * 10 + a;
+      }
+
+      return joltage;
+    }),
+  );
 }
