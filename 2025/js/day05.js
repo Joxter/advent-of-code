5100d5c324287c695b0d1675b7f40d2e84da1371
Joxter <joxterrr@gmail.com>
2025-12-05 17:20:23 +0100

    2025 5 p2

--- 2025/js/day05.js
+++ 2025/js/day05.js
@@ -1,25 +1,11 @@
-import { runDay } from "../../utils.js";
+import { runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2025/day/5
-
-console.log(
-  part1(`3-5
-10-14
-16-20
-12-18
-
-1
-5
-8
-11
-17
-32`),
-);
 
 runDay(2025, 5)
   //
   .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -34,5 +20,35 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let ranges = inp
+    .split("\n\n")[0]
+    .split("\n")
+    .map((l) => {
+      return l.split("-").map((n) => +n);
+    })
+    .toSorted((a, b) => {
+      return a[0] - b[0];
+    });
+
+  let from = ranges[0][0];
+  let to = ranges[0][1];
+  let total = 0;
+
+  let limit = 1000;
+  ranges.shift();
+
+  while (limit-- > 0 && ranges.length) {
+    let range = ranges.shift();
+
+    if (range[0] > to) {
+      total += to - from + 1;
+      from = range[0];
+      to = range[1];
+    } else {
+      to = Math.max(range[1], to);
+    }
+  }
+  total += to - from + 1;
+
+  return total;
 }
