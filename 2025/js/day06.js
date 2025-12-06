530f10c5631b232f105aa72018604c62f99ada06
Joxter <joxterrr@gmail.com>
2025-12-06 07:47:27 +0100

    2025 6 p2, wip

--- 2025/js/day06.js
+++ 2025/js/day06.js
@@ -1,33 +1,26 @@
-import { runDay, sum } from "../../utils.js";
+import { prod, rotateGrid90, runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2025/day/6
 
-// console.log(
-//   part1(`123 328  51 64
-//  45 64  387 23
-//   6 98  215 314
-// *   +   *   +`),
-//   [4277556],
-// );
+console.log(
+  part2(`123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +`),
+  [3263827],
+);
 
 runDay(2025, 6)
   //
-  .part(1, part1) // 6661595817617 hi
-  // .part(2, part2)
+  // .part(1, part1)
+  // .part(2, part2) // 12377436852367 low
   .end();
 
 function part1(inp) {
-  // console.log(inp);
   let [a, b, c, d, acts] = inp.split("\n").map((l) => l.trim().split(/\s+/));
-  // console.log(a);
-  // console.log(b);
-  // console.log(c);
-  // console.log(d);
-  // console.log(acts);
 
   return sum(
     acts.map((act, i) => {
-      // console.log(act, [+a[i], +b[i], +c[i]]);
       if (act === "+") {
         return +a[i] + +b[i] + +c[i] + +d[i];
       } else {
@@ -38,5 +31,36 @@ function part1(inp) {
 }
 
 function part2(inp) {
-  return 123;
+  let grid = rotateGrid90(
+    inp.split("\n").map((row, i, all) => {
+      // console.log(row.length);
+      return row.padEnd(3724, " ");
+    }),
+  );
+  // console.log(grid.slice(-20));
+
+  let aaa = [];
+
+  let act = grid[0][0];
+  let nums = [];
+
+  grid.forEach((row) => {
+    act = row[0] === "*" ? "*" : row[0] === "+" ? "+" : act;
+
+    let n = +row.slice(1).reverse().join("").trim();
+    // console.log(act, n);
+
+    if (n === 0) {
+      if (act === "+") {
+        aaa.push(sum(nums));
+      } else {
+        aaa.push(prod(nums));
+      }
+      nums = [];
+    } else {
+      nums.push(n);
+    }
+  });
+
+  return sum(aaa);
 }
