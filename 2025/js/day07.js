2343ae6c6de82aa53ef7a3239bc8e1b86d74d574
Joxter <joxterrr@gmail.com>
2025-12-07 09:42:04 +0100

    2025 7 p2

--- 2025/js/day07.js
+++ 2025/js/day07.js
@@ -1,33 +1,11 @@
-import { findInGrid, makeGrid, runDay } from "../../utils.js";
+import { findInGrid, makeGrid, runDay, sum } from "../../utils.js";
 
 // https://adventofcode.com/2025/day/7
-
-console.log(
-  part1(
-    `
-.......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............`.trim(),
-  ),
-);
 
 runDay(2025, 7)
   //
   .part(1, part1)
-  // .part(2, part2)
+  .part(2, part2)
   .end();
 
 function part1(inp) {
@@ -51,15 +29,32 @@ function part1(inp) {
     if (newBeams.length > 0) {
       beams = newBeams;
     }
-
-    // console.log(
-    //   grid[row].map((c, i) => (beams.includes(i) ? "|" : c)).join(""),
-    // );
   }
 
   return split;
 }
 
 function part2(inp) {
-  return 123;
+  let grid = makeGrid(inp);
+  let [row, b] = findInGrid(grid, "S");
+  let beams = { [b]: 1 };
+
+  for (let row = 1; row < grid.length; row++) {
+    let newBeams = {};
+
+    for (let i = 0; i < grid[row].length; i++) {
+      if (grid[row][i] === "^" && beams[i]) {
+        newBeams[i - 1] = (newBeams[i - 1] || 0) + beams[i];
+        newBeams[i + 1] = (newBeams[i + 1] || 0) + beams[i];
+      } else if (grid[row][i] === "." && beams[i]) {
+        newBeams[i] = (newBeams[i] || 0) + beams[i];
+      }
+    }
+
+    if (Object.keys(newBeams).length > 0) {
+      beams = newBeams;
+    }
+  }
+
+  return sum(Object.values(beams));
 }
