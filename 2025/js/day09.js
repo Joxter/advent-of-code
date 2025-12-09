68acb1c78c337a7bde71770d41ff27e87444bd0b
Joxter <joxterrr@gmail.com>
2025-12-09 20:52:39 +0100

    2025 9 p2 clean and optimised

--- 2025/js/day09.js
+++ 2025/js/day09.js
@@ -23,30 +23,34 @@ function part1(inp) {
 }
 
 function part2(inp) {
+  // not a general solution, but should for work all inputs
   let max = 0;
   let reds = inp.split("\n").map((l) => ints(l));
+  const maxCoord1 = Math.max(...reds.map((r) => r[0]));
+  const maxCoord = Math.max(maxCoord1, ...reds.map((r) => r[1]));
 
-  let ghost = [
-    [10000, 50058],
-    [20000, 50058],
-    [30000, 50058],
-    [40000, 50058],
-    [50000, 50058],
-    [60000, 50058],
-    [70000, 50058],
-    [80000, 50058],
-  ];
+  let rootDots = [];
+  let ghost = [];
 
-  for (let i = 0; i < reds.length - 1; i++) {
-    loop1: for (let j = i + 1; j < reds.length; j++) {
-      let a = reds[i];
+  reds.forEach((red, i, all) => {
+    let prev = all[i - 1] || all.at(-1);
+    if (prev) {
+      let len = Math.abs(red[0] - prev[0]);
+      if (len > maxCoord >> 1) {
+        ghost.push([(prev[0] + red[0]) >> 1, (prev[1] + red[1]) >> 1]);
+        rootDots.push(prev, red);
+      }
+    }
+  });
+
+  for (let i = 0; i < rootDots.length; i++) {
+    loop1: for (let j = 0; j < reds.length; j++) {
+      let a = rootDots[i];
       let b = reds[j];
       let verMin = Math.min(a[0], b[0]) + 1;
       let verMax = Math.max(a[0], b[0]) - 1;
       let horMin = Math.min(a[1], b[1]) + 1;
       let horMax = Math.max(a[1], b[1]) - 1;
-
-      if (verMin > verMax || horMin > horMax) continue;
 
       for (let k = 0; k < reds.length; k++) {
         if (
