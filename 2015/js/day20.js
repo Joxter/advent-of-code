6ef81927ec1a56a41a8930fe6b1fd7d525e8bd39
Joxter <joxterrr@gmail.com>
2025-12-20 21:35:48 +0100

    2015 d20, optimisations

--- 2015/js/day20.js
+++ 2015/js/day20.js
@@ -4,33 +4,28 @@ import { runDay } from "../../utils.js";
 
 runDay(2015, 20)
   //
-  .part(1, part1)
-  .part(2, part2)
-  .part(1, part1reddit, 'reddit')
+  // .part(1, part1) // 2min :(
+  // .part(2, part2)
+  .part(1, part1reddit, "reddit")
   .part(2, part2reddit, 'reddit')
+  .part(1, part1redditEnh, "reddit enh")
+  .part(2, part2redditEnh, "reddit enh")
   .end();
 
 function part1(inp) {
   let target = +inp;
 
   let i = 0;
-  let max = 0;
-  let prevMaxI = 0;
-  let step = 2;
-  while (i < 1_000_000) {
-    i += step;
+  while (i < target / 10) {
+    i += 2;
     let cnt = i * 10;
 
-    for (let j = 1; j <= i * 0.5; j++) {
+    for (let j = 1; j <= i >> 1; j++) {
       if (i % j === 0) {
         cnt += j * 10;
       }
     }
 
-    if (cnt > max) {
-      prevMaxI = i;
-      max = cnt;
-    }
     if (cnt > target) return i;
   }
 }
@@ -38,24 +33,17 @@ function part1(inp) {
 function part2(inp) {
   let target = +inp;
 
-  let i = 819000;
-  let max = 0;
-  let prevMaxI = 0;
-  let step = 2;
+  let i = 0;
   while (i < 1_000_000) {
-    i += step;
+    i += 2;
     let cnt = i * 11;
 
-    for (let j = 1; j <= i * 0.5; j++) {
+    for (let j = 1; j <= i >> 1; j++) {
       if (i % j === 0 && i / j <= 50) {
         cnt += j * 11;
       }
     }
 
-    if (cnt > max) {
-      prevMaxI = i;
-      max = cnt;
-    }
     if (cnt > target) return i;
   }
 }
@@ -96,4 +84,45 @@ function part2reddit(input) {
       min === 0 && current >= input ? (min = index) : min,
     0,
   );
+}
+
+function part1redditEnh(inp) {
+  inp = +inp; // LOL this line gives x10 perf boost
+  const presents = Array(inp / 20).fill(10);
+
+  for (let elf = 2; elf < inp / 10; elf++) {
+    let step = elf % 2 === 0 ? elf : elf * 2;
+
+    for (let i = step; i < inp / 10; i += step) {
+      presents[i / 2] += elf * 10;
+    }
+  }
+
+  for (let i = 0; i < presents.length; i++) {
+    if (presents[i] >= inp) {
+      return i * 2;
+    }
+  }
+}
+
+function part2redditEnh(inp) {
+  inp = +inp;
+  const presents = Array(inp / 20).fill(11);
+
+  for (let elf = 2; elf < inp / 10; elf++) {
+    let visits = 0;
+    let step = elf % 2 === 0 ? elf : elf * 2;
+    let vis = elf % 2 === 0 ? 1 : 2;
+
+    for (let i = step; i < inp / 10 && visits < 50; i += step) {
+      if (i % 2 === 0) presents[i / 2] += elf * 11;
+      visits += vis;
+    }
+  }
+
+  for (let i = 0; i < presents.length; i++) {
+    if (presents[i] >= inp) {
+      return i * 2;
+    }
+  }
 }
