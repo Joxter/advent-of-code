a90f7f932b466f1e934a0bb73ab9d1ca0cf6cec8
Joxter <joxterrr@gmail.com>
2026-01-03 18:02:47 +0100

    2016 d11 optimisation

--- 2016/python/day11.py
+++ 2016/python/day11.py
@@ -6,7 +6,7 @@ def get_ones5(n: int):
     i = 0
     while n:
         if n & 1:
-            arr.append(i)
+            arr.append(1 << i)
         n = n >> 1
         i += 1
 
@@ -93,14 +93,7 @@ def _is_valid7(floor: int):
 
 
 def part1(inp: str):
-    """
-    The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
-    The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
-    The third floor contains a thulium-compatible microchip.
-    The fourth floor contains nothing relevant.
-    """
     el = 1
-
     floors = [
         0,
         0b11000_11000,  # 1
@@ -109,100 +102,67 @@ def part1(inp: str):
         0b00000_00000,  # 4
     ]
 
-    # floors = [
-    #     0,
-    #     0b00000_00011,  # 1
-    #     0b00010_00000,  # 2
-    #     0b00001_00000,  # 3
-    #     0b00000_00000,  # 4
-    # ]
-
-    # limit = 100_000_000
-    limit = 10000000_000_000
     q = [(floors, el, 0)]
     visited = set()
 
-    while limit > 0 and len(q) > 0:
-        limit -= 1
-
+    while len(q) > 0:
         floors, el, steps = q.pop(0)
-
-        if limit % 100_000 == 0:
-            print(len(q), len(visited))
-            print_floors(floors)
-            print((join(",", floors), el, steps))
 
         if floors[4] == 0b11111_11111:
             return steps
 
-        if not is_valid5(floors):
-            continue
-
         kk = f"{floors[1]},{floors[2]},{floors[3]},{floors[4]}"
         if (kk, el) in visited:
             continue
-
         visited.add((kk, el))
 
-        f = floors[el]
+        ones = get_ones5(floors[el])
+        for one in ones:
+            # up
+            if el < 4:
+                new_f = floors.copy()
+                new_f[el] = one ^ new_f[el]
+                new_f[el + 1] = one | new_f[el + 1]
 
-        # print(f"    {f:10b}", limit)
-        # print_floors(floors)
+                if _is_valid5(new_f[el]) and _is_valid5(new_f[el + 1]):
+                    q.append((new_f, el + 1, steps + 1))
 
-        if f != 0:
-            ones = get_ones5(f)
-            # print(ones)
-            for one in ones:
+            # down
+            if el > 1 and floors[el - 1]:
+                new_f = floors.copy()
+                new_f[el] = one ^ new_f[el]
+                new_f[el - 1] = one | new_f[el - 1]
+
+                if _is_valid5(new_f[el]) and _is_valid5(new_f[el - 1]):
+                    q.append((new_f, el - 1, steps + 1))
+
+        for a in range(len(ones) - 1):
+            for b in range(a + 1, len(ones)):
+                two = ones[a] | ones[b]
+
                 # up
                 if el < 4:
                     new_f = floors.copy()
-                    new_f[el] = (1 << one) ^ new_f[el]
-                    new_f[el + 1] = (1 << one) | new_f[el + 1]
+                    new_f[el] = two ^ new_f[el]
+                    new_f[el + 1] = two | new_f[el + 1]
 
-                    q.append((new_f, el + 1, steps + 1))
-
-                # down
-                if el > 1:
-                    new_f = floors.copy()
-                    new_f[el] = (1 << one) ^ new_f[el]
-                    new_f[el - 1] = (1 << one) | new_f[el - 1]
-
-                    q.append((new_f, el - 1, steps + 1))
-
-            # continue
-
-            for a in range(len(ones) - 1):
-                for b in range(a + 1, len(ones)):
-                    two = (1 << ones[a]) | (1 << ones[b])
-
-                    # up
-                    if el < 4:
-                        new_f = floors.copy()
-                        new_f[el] = two ^ new_f[el]
-                        new_f[el + 1] = two | new_f[el + 1]
-
+                    if _is_valid5(new_f[el]) and _is_valid5(new_f[el + 1]):
                         q.append((new_f, el + 1, steps + 1))
 
-                    # down
-                    if el > 1:
-                        new_f = floors.copy()
-                        new_f[el] = two ^ new_f[el]
-                        new_f[el - 1] = two | new_f[el - 1]
+                # down
+                if el > 1 and floors[el - 1]:
+                    new_f = floors.copy()
+                    new_f[el] = two ^ new_f[el]
+                    new_f[el - 1] = two | new_f[el - 1]
 
+                    if _is_valid5(new_f[el]) and _is_valid5(new_f[el - 1]):
                         q.append((new_f, el - 1, steps + 1))
 
     return None
 
 
 def part2(inp: str):
-    """
-    The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
-    The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
-    The third floor contains a thulium-compatible microchip.
-    The fourth floor contains nothing relevant.
-    """
     el = 1
-
     floors = [
         0,
         0b1111000_1111000,  # 1
@@ -211,25 +171,14 @@ def part2(inp: str):
         0b0000000_0000000,  # 4
     ]
 
-    limit = 10000000_000_000
     q = [(floors, el, 0)]
     visited = set()
 
-    while limit > 0 and len(q) > 0:
-        limit -= 1
-
+    while len(q) > 0:
         floors, el, steps = q.pop(0)
 
         if floors[1] and steps > 15:
             continue
-
-        if not is_valid7(floors):
-            continue
-
-        if limit % 50_000 == 0:
-            print(len(q), len(visited))
-            print_floors(floors)
-            print((join(",", floors), el, steps))
 
         if floors[4] == 0b1111111_1111111:
             return steps
@@ -241,46 +190,49 @@ def part2(inp: str):
 
         f = floors[el]
 
-        if f != 0:
-            ones = get_ones7(f)
-            # print(ones)
-            for one in ones:
+        if f == 0:
+            continue
+
+        ones = get_ones7(f)
+        for one in ones:
+            # up
+            if el < 4:
+                new_f = floors.copy()
+                new_f[el] = one ^ new_f[el]
+                new_f[el + 1] = one | new_f[el + 1]
+
+                if _is_valid7(new_f[el]) and _is_valid7(new_f[el + 1]):
+                    q.append((new_f, el + 1, steps + 1))
+
+            # down
+            if el > 1 and floors[el - 1]:
+                new_f = floors.copy()
+                new_f[el] = one ^ new_f[el]
+                new_f[el - 1] = one | new_f[el - 1]
+
+                if _is_valid7(new_f[el]) and _is_valid7(new_f[el - 1]):
+                    q.append((new_f, el - 1, steps + 1))
+
+        for a in range(len(ones) - 1):
+            for b in range(a + 1, len(ones)):
+                two = ones[a] | ones[b]
+
                 # up
                 if el < 4:
                     new_f = floors.copy()
-                    new_f[el] = (1 << one) ^ new_f[el]
-                    new_f[el + 1] = (1 << one) | new_f[el + 1]
+                    new_f[el] = two ^ new_f[el]
+                    new_f[el + 1] = two | new_f[el + 1]
 
-                    q.append((new_f, el + 1, steps + 1))
-
-                # down
-                if el > 1:
-                    new_f = floors.copy()
-                    new_f[el] = (1 << one) ^ new_f[el]
-                    new_f[el - 1] = (1 << one) | new_f[el - 1]
-
-                    q.append((new_f, el - 1, steps + 1))
-
-            # continue
-
-            for a in range(len(ones) - 1):
-                for b in range(a + 1, len(ones)):
-                    two = (1 << ones[a]) | (1 << ones[b])
-
-                    # up
-                    if el < 4:
-                        new_f = floors.copy()
-                        new_f[el] = two ^ new_f[el]
-                        new_f[el + 1] = two | new_f[el + 1]
-
+                    if _is_valid7(new_f[el]) and _is_valid7(new_f[el + 1]):
                         q.append((new_f, el + 1, steps + 1))
 
-                    # down
-                    if el > 1:
-                        new_f = floors.copy()
-                        new_f[el] = two ^ new_f[el]
-                        new_f[el - 1] = two | new_f[el - 1]
+                # down
+                if el > 1 and floors[el - 1]:
+                    new_f = floors.copy()
+                    new_f[el] = two ^ new_f[el]
+                    new_f[el - 1] = two | new_f[el - 1]
 
+                    if _is_valid7(new_f[el]) and _is_valid7(new_f[el - 1]):
                         q.append((new_f, el - 1, steps + 1))
 
     return None
@@ -288,7 +240,7 @@ def part2(inp: str):
 
 run_day(2016, 11).parts(
     [
-        [1, part1],
+        # [1, part1],
         [2, part2],
     ]
 ).end(1)
